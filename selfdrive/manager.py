f3bf7070e0f5b386b7c5dcc4657c2b7112fd9749
Andy Haden <andy@comma.ai>
2020-02-03 12:23:04 -0800

    manager: verify daemon process cmdline
    old-commit-hash: 9b1f2d5c64320a9b6eca378d4b1876850504c525

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3cadaca03f9ee1a49920505ae5a2d964ae39ef48774117c8d4fe3efb5963027
-size 15569
+oid sha256:00591086aec962ce7389fe4baedf30aaa4211e1125bb124e0eeefbffd4a717b3
+size 15653
