926ccc5534aa23f4b38526ec37eab5c7d9dcab95
Willem Melching <willem.melching@gmail.com>
2020-02-14 17:31:27 -0800

    Fix waiting for unkillable process. Fixes #1087 (#1099)
    * Fix waiting for unkillable process. Fixes #1087
    
    * Add bugfix to release notes
    
    * Don't pass in exitcode
    
    old-commit-hash: 909efef6af6d8d396ea9c8e1122455231a062682

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad404733929eeb460289d4c06a1500609878c2b1f5e5e9cb5afd0b416ba2533f
-size 15740
+oid sha256:2bd6b28fef4bb3367667cc55ff6f8e5e1b0744772480cc7aa2e8456cf9a989f7
+size 15780
