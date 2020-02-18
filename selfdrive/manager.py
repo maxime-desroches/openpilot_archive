b0167ca76516d8938c727dfbafe58c554b7f0794
Willem Melching <willem.melching@gmail.com>
2020-02-17 18:07:23 -0800

    Run locationd online (#1089)
    * add locationd to manager
    
    * add to release files and make linter happy
    
    * Don't ship all models
    
    * Also add locationd.py
    
    * Remove unused import
    
    * Fix import in kalman helpers
    
    old-commit-hash: ff91c6f9d47a37e2475570bc4d370221379f49be

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:022bc80dbe9e6c27caaf33c699b6fe47499eebe31c3794fe450623465eee95af
-size 15780
+oid sha256:efb3726cfc127ebfacb15f5428263485be4edabebca8e634d3d1970327aa8ecf
+size 15843
