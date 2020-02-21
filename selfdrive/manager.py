505936bc943df4110c2d0efbe54de83abcca164b
Adeeb <8762862+quillford@users.noreply.github.com>
2020-02-20 16:22:25 -0800

    Abstract common events + event cleanup (#1129)
    * too many if
    
    * unused
    
    * whitespace
    
    * key
    
    * sefldrive/car/*
    
    * no more gctx
    
    * lower
    
    * start abstracting common events
    
    * all cars
    
    * start small
    
    * all cars
    
    * reverse gear
    
    * wrongCarMode
    
    * wrongGear
    
    * espDisabled
    
    * steerUnvailable
    
    * make linter happy
    
    * c isn't used
    
    * fix esp_disabled in VW
    
    * update ref
    
    * more red
    
    * more cleanup
    
    * fix subaru
    
    * update ref
    
    old-commit-hash: e8cb6ea06a2470dd89a6ad8fd17bd1d1b62034cc

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:efb3726cfc127ebfacb15f5428263485be4edabebca8e634d3d1970327aa8ecf
-size 15843
+oid sha256:31ce04f9d7d877fff456425e91d431e24d0aa764d605664cfa92a0f7e81e65c3
+size 15068
