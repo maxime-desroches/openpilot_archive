afcc2b38610a39f40d0e8035d8fa5350024fb4c6
George Hotz <geohot@gmail.com>
2020-03-01 09:57:28 -0800

    Revert "[Memory leak]uniq_ptr for tmsg (#985)"
    This reverts commit b1f3fbd71f40c2142e6aaddd3b5579413a1412a3.
    
    old-commit-hash: 86b1ca176a2c4e86cb511e7e34694621950c6c90

--- tools/nui/FileReader.cpp
+++ tools/nui/FileReader.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0315dbc0aaf4ee75d8cd63d2030ef0638bd7ffcdda5af764d7cae98f2a5f91f6
-size 3868
+oid sha256:43773404b67efa48a475f52d42fcb5512d2d6e4c7fa3f720a3b32347b1ace1bd
+size 3843
