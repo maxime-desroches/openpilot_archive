219ffb861cc869d3a7a8855b476c5bc0278b01d2
Andrew Valish <valish@users.noreply.github.com>
2020-03-16 13:02:45 -0700

    fix sidebar shadow in network settings (#1245)
    old-commit-hash: 0aafd67b56a4bac08c5c84e95cad044e5c41dc80

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:32408d1ff1aac2ebf5b7dd08dfa0adad659182c6f944d545fa6efcc0fe1a8e23
-size 35255
+oid sha256:9bcb5ce48e6e2374102a0a178e02a1b8a96bc9cc425c0dde0dc4c659635299cb
+size 35336
