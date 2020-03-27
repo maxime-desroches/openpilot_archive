1a8096d00969c72564061b07b11d5d1bc6677c62
eFini <ricklan@gmail.com>
2020-03-28 04:00:42 +1000

    paint.cc uses color defined in ui.hpp (#1261)
    * pain.cc uses color defined in ui.hpp
    
    * use macro
    old-commit-hash: 47f17622d9e4991beec73df40a3ffcc0654a3e09

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bbe9d4a355dfce0bd4c0a4f2a1a4cfeb8db06734ff3b31b8ff05b3ee1a137979
-size 35444
+oid sha256:752698defedb252307845616c98e6702b62958b36e32fd21da41c4dad3413efd
+size 35182
