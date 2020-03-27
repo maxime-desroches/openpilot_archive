2d13600bfd6131879e3b1d1b7df187cd14adeb5f
Harald Schafer <harald.the.engineer@gmail.com>
2020-03-26 17:40:12 -0700

    add long_x
    old-commit-hash: d02f55ae0b0ee39ed758c5b8a19411fddc4905a4

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5d7f6639f2b10fe26aa2ebece4b39ec8ca8815130d85279b463cb016e520ace
-size 11050
+oid sha256:f01a219c9f8b46e71f5b350eff3abf2919f62113f5152571cf8a85924dc901b9
+size 11267
