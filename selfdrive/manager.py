9a19b015ec56c99f7dcac3abba8b83a8068e2a02
ZwX1616 <zwx1616@gmail.com>
2020-03-26 19:19:47 -0700

    add webcam to cameras (#1201)
    old-commit-hash: a95e61edf4176c034c0537293a3cf0753c45daa1

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:181a354bd0c5d8f53e8d577d1405a974511262d66e50e19f54d858d6ff108bd5
-size 16016
+oid sha256:cd564487caafd40be748af446892d47b537f186eb46342baf69b9ce87e717950
+size 16126
