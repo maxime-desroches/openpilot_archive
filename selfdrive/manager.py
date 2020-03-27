ca613b04433b79026276b1f9bad822b5c85b2440
Andy <andy@comma.ai>
2020-03-26 19:49:58 -0700

    Replace usages of datetime.now() with datetime.utcnow() (#1267)
    old-commit-hash: 54f34abdb36cbf7a43b3de1ba8765a972413179d

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd564487caafd40be748af446892d47b537f186eb46342baf69b9ce87e717950
-size 16126
+oid sha256:3ea86b7191a7ae335d9efbaf4086eaff53df9eeb24ee209f9c30ae2debb4854c
+size 16129
