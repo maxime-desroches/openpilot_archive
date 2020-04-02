45a2cf792e835f7376093df50658eaf76e6419b7
Willem Melching <willem.melching@gmail.com>
2020-04-02 11:28:20 -0700

    Direct logentries logging from manager (#1306)
    * add function to add direct logentries handler
    
    * Whitespace
    
    * Add manager crash handler
    
    * Log scons build errors
    
    * Add line to format exception
    
    * Small cleanup
    
    * That is now a list
    
    * Read all data from stderr
    old-commit-hash: c95134defe6734dcffeff6f9a1563f9ca8475722

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b9d004cb0c40141c6ba7bafb95fb18a4b8a049d740ff095f3ebf76e0a23a723
-size 16100
+oid sha256:626a946c1f5cdd9e8f6ce4b3d6d46b4697d0946fada5431361c1bda89068876f
+size 16857
