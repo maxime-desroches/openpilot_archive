6c249fbbc64eb9708ffbfe72f2a3928892fa7bd3
Willem Melching <willem.melching@gmail.com>
2020-04-02 15:04:01 -0700

    Show manager startup failures using TextWindow (#1310)
    * show text window on build failure
    
    * Only show last 10 line
    
    * Fix that
    
    * better layout
    
    * Fix fake text window
    
    * Only show spinner and textwindow on android
    
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: dae19a284d20a81432c57f99be8ac69bb3c2454a

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:626a946c1f5cdd9e8f6ce4b3d6d46b4697d0946fada5431361c1bda89068876f
-size 16857
+oid sha256:c5a8a97c64f1fe8562d319c714ada9f8c09bb72b5a8852aa8c2084cb92b06683
+size 17292
