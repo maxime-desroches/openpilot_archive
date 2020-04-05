fb1427e56dafc71f4202a1434ccffae4e25d47bd
Andy <andy@comma.ai>
2020-04-05 11:06:49 -0700

    Sidebar Connectivity Status (#1268)
    * store athena connected-at time
    
    * refactor
    
    * deref
    
    * dt import
    
    * Athena status based on last ping time param
    
    * upd test for pingtime
    
    * lower error timeout
    old-commit-hash: 41bb3ac7ca586b41fa91ad5a6392c88a2c4d5cb3

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c60210b961b28e4cd7e2a27b5f8b5ea3c9a985fe0bfe9a2d8815bc4b05562290
-size 35605
+oid sha256:a0c1db802475d1a01323228de8a3e4e3af5c6d00abcaedb4a1205fe774e4d7a7
+size 35613
