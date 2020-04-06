131b526c2094d9c3a84ec66f123efdb126344e13
Shane Smiskol <shane@smiskol.com>
2020-04-06 13:52:15 -0500

    few small spinner updates (#1319)
    old-commit-hash: 10331b1cf86659a6ad32137debc43374e4feece8

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c5a8a97c64f1fe8562d319c714ada9f8c09bb72b5a8852aa8c2084cb92b06683
+oid sha256:e321f5f8ac0fd9f8f853d69d09037c014c48d08a14d2436094b33dbac572d8f9
 size 17292
