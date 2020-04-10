f7c407d44b98e0b56f1ff3544bf96f0925455472
George Hotz <72895+geohot@users.noreply.github.com>
2020-04-09 20:55:54 -0700

    Remove bg_thread from ui (#1347)
    * remove bg_thread
    
    * debug print app
    
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: ef8aaa77b79194cd0bc25b1e2a42ca857c34a401

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:769523bedeb049739fc3ab00d8ae8d7bd83d400afcb9c14c4d85a76738c4fced
-size 35521
+oid sha256:2b14217811005fb5a08fce6b93b2587ef9272f685591d7cc5315bd87361b524e
+size 35167
