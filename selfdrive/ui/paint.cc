eb6ee8b626a6d23bfa18fd70be44f65bb61e2f1a
Dean Lee <deanlee3@gmail.com>
2020-04-14 06:38:45 +0800

    add ui_draw_image to reduce code repetition
    old-commit-hash: 1ecef2c6be9e47ab5942ef3c4bf48e9baf6af921

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:754ebbf5984c455bb51d3e4bb595019be2e1a27669a7a774aa46e6738dee8b8d
-size 35192
+oid sha256:9fead82640790b122faf351a3860fed269741014f1672b4530e32d2dccf5b736
+size 34624
