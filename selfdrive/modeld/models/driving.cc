f2fdcc11712bdce3763115f6500b1ab6ae3a45ce
HaraldSchafer <harald.the.engineer@gmail.com>
2020-04-15 21:19:25 -0700

    add traffic convention (#1372)
    * I think this works
    
    * clean up
    old-commit-hash: f5b28ad2d267adae0d6e6fd7816389d3f8d32200

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ee7dcef291294bf114aa9f9f640608312e6ed445d59bf923b1a801290bc8cb0
-size 11813
+oid sha256:2cc2b01bd589442fa8431c8d7c2bbfdb130ec411e63c31f4f3c0da884977c0f0
+size 11889
