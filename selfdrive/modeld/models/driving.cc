b0c6a93cd849de207ed458a413c7b0bb6bff827d
Dean Lee <deanlee3@gmail.com>
2020-04-23 01:16:05 +0800

    unmap a previously mapped buff (#1410)
    old-commit-hash: 98ec06e125b605405900673be0305677ad5efce3

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2cc2b01bd589442fa8431c8d7c2bbfdb130ec411e63c31f4f3c0da884977c0f0
-size 11889
+oid sha256:cbe2dd672a70f04a67917611b1180449a2d0aad0eb9afae390100fac1b98765f
+size 11977
