540272ad254e34da14c071e376892dd1b840f1ce
Dean Lee <deanlee3@gmail.com>
2020-04-29 04:44:57 +0800

    replace duplicate codes with variable (#1431)
    * eplace duplicate codes with variable
    
    * remove space
    old-commit-hash: b2be607974f4b51eba9832d9d242662a128b863f

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f413dcefdcd328c066373bc59bc0c9213b061af0f38f28b09bc8ee4245273bab
-size 37522
+oid sha256:b3e5542a4141b114b7de6e72e6861ba6c2d42a3aa4c05d6196b609da89ca2600
+size 37419
