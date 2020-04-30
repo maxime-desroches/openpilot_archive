fed2c40c7c3dcb7be5f0f05a02651d09bee827d5
Dean Lee <deanlee3@gmail.com>
2020-05-01 03:11:35 +0800

    better ui_draw() (#1432)
    * better ui_draw()
    
    * fix missed border
    
    reduce code
    
    remove white line
    old-commit-hash: 3fe3afeedeab176f7423a73c0f573396eef41d45

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3e5542a4141b114b7de6e72e6861ba6c2d42a3aa4c05d6196b609da89ca2600
-size 37419
+oid sha256:2b18b08a853983b5cbb2d621ce9ab2aacdf80a70789a5cd242b71141cb0d4c3a
+size 37092
