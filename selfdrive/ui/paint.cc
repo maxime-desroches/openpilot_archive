49a84237006c5d05a59609ced349fea1155201e7
Dean Lee <deanlee3@gmail.com>
2020-05-02 10:16:17 +0800

    simplify drawing circle images (#1445)
    * simplify drawing circle images
    
    add static
    
    set default color to 0 0 0 0
    
    remove lines
    
    * fix img_weel_y
    
    * use color macro
    old-commit-hash: 4857f45d6a0236f9977b8848a48c1d883cf0e837

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b18b08a853983b5cbb2d621ce9ab2aacdf80a70789a5cd242b71141cb0d4c3a
-size 37092
+oid sha256:e50219278d4023679ca154d31846f1a2e0b0ccf5bf1deedb2519beb4fca158a1
+size 35359
