9ea4e2fd4d6eabe36c0f3e556c543028f5b40193
HaraldSchafer <harald.the.engineer@gmail.com>
2020-05-08 17:14:43 -0700

    Better poly (#1437)
    - better polyfitting
    - no mpc cost change during lane change
    - model trained with better ll gt, sim noise and guaranteed memories
    old-commit-hash: 65fcc7c45ff1d4d747dc52ff5791afb84a18744a

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cbe2dd672a70f04a67917611b1180449a2d0aad0eb9afae390100fac1b98765f
-size 11977
+oid sha256:754ad10966e18cc8441f787ba57bb7303b089aa7261c77bd33949e71c310987c
+size 11907
