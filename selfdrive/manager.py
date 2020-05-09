95f4085bd397c53a039504dc65c394b0bdae2033
George Hotz <geohot@gmail.com>
2020-05-08 20:30:57 -0700

    remove locationd from car started processes for now
    old-commit-hash: 7c71a14a89484ff713019c0a7983baeec7582061

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86413000520759c2e5865c29f64fdbad0f574d0ffd3eb6770d81298609a6e5f9
-size 17703
+oid sha256:45400a1ada03c30bc975de8d4aee39764ec58f1346d0a1738bac9d79dc67ad4e
+size 17704
