b54a8b8b38fed459a5fc20f5ed252b91801cfcae
Willem Melching <willem.melching@gmail.com>
2020-05-13 16:41:52 -0700

    remove eigen from phonelibs (#1505)
    old-commit-hash: add784a7df0bd27aa1df115e38d43d5159c832f9

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bfe5bb4883b23dbc6134946600d408d68edf5c63e8edef8b7b31f8206d5df9a8
-size 11915
+oid sha256:f5d8f71dbac441b7844681712a48f34db80742bfba08d3dce868e69f49468520
+size 11895
