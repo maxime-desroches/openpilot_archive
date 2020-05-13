4b45b4e3ca13c06026c627a85aa0a270ec341716
Willem Melching <willem.melching@gmail.com>
2020-05-12 18:17:04 -0700

    Fix steering wheel icon showing (#1497)
    * this should fix steering wheel icon showing
    
    * fix syntax
    old-commit-hash: 2ae2c40b2943c502c4c66a44ac6ae5cb773c208b

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6716c31f292c353f9a300e67fadc535c25fc5c94082596f4db9e7b368fd1e28
-size 33883
+oid sha256:3a6becfd73be7f8d783e9988e466359a00115ebb64f4c2f63a01f540cdebb216
+size 33892
