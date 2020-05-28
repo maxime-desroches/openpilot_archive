3a6d57d3e3d6dc968857dfc0c2769c46e80f8848
Willem Melching <willem.melching@gmail.com>
2020-05-28 15:05:04 -0700

    Run mypy commit hook (#1591)
    * run mypy commit hook
    
    * fix mypy errors
    old-commit-hash: 3d08dcc3b27936cb14c0eae63605be9a6c077380

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86413000520759c2e5865c29f64fdbad0f574d0ffd3eb6770d81298609a6e5f9
-size 17703
+oid sha256:2ee050f7fcca6c137264cff65e9602f079fda4ba5dce1b1e252457dedec61f41
+size 17798
