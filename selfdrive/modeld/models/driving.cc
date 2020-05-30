3470b992feac8ecf1c67a930ce16c13ed4cc7154
Dean Lee <deanlee3@gmail.com>
2020-05-31 01:08:44 +0800

    driving.cc: Pass ModelDataRaw by reference (#1600)
    * pass ModelDataRaw by reference
    
    * pass NVGpaint by refer
    old-commit-hash: af36aa28abeb55a4445cbc1ff13ac55993d989d9

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b26f01dd72a2b8c1e27a401f52353e20e4b5163a068712053045b7249800d73a
-size 11665
+oid sha256:92fab759b333a3929fe41f1f841dff4b66be4a1489ffcb501588e273dea407ab
+size 11667
