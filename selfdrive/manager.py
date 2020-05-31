9a6cba481385eb3b9c6fff4f51f9d0af5e1d5c44
Willem Melching <willem.melching@gmail.com>
2020-05-31 13:36:27 -0700

    Enable new params learner for improved vehicle model estimation
    old-commit-hash: a0adc4482108a3b89b6e4cb3fe27b8f51291bc08

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33039eb4ae1a3dfd1373193fe155e6a88c8dbc4b1e0791be4a1abc0c3927e938
-size 17800
+oid sha256:a6a7567638b71056e14ea1485c0f7f9f58906714019f43f0ec28178ecf161176
+size 17791
