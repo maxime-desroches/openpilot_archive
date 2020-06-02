9586a91d5ae875a7c78dc2cbadb8f1d8b4d8cd71
Willem Melching <willem.melching@gmail.com>
2020-06-02 16:29:32 -0700

    Make pylint more strict (#1626)
    * make pylint more strict
    
    * cleanup in progress
    
    * done cleaning up
    
    * no opendbc
    old-commit-hash: 843a64c72fb94397a1edf814fa66044c8d92acbf

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b620a18348c2cd33f4455b53c21c9a934050588b038e60f8ecafb7c653bc4e28
-size 17792
+oid sha256:214be1cc9c39f81f5b54b21ca17f36d11b7081ed8d8db7f002af71a42be1c503
+size 17812
