a8e4770875f722c1a56026b76c7d8210ce52c27c
George Hotz <geohot@gmail.com>
2020-06-03 14:18:42 -0700

    save valid_len
    old-commit-hash: 4e7fe3a9ec4fddd517ea470af3b7eb3a6fd415e3

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92fab759b333a3929fe41f1f841dff4b66be4a1489ffcb501588e273dea407ab
-size 11667
+oid sha256:9cfc4371487acc5e018f681a5a617c2493ea354c64ae21c330d529be7c41d52d
+size 11619
