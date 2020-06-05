cde7159ba480956a81096770e42edee3f16d1d75
Dean Lee <deanlee3@gmail.com>
2020-06-06 06:33:45 +0800

    ui.cc: remove variables from UIScene, use event::Reader to get data  (#1585)
    * use struct to store lead status
    
    * remove white space
    
    * use RadarState::LeadData::Reader
    
    * use reader to get thermal data
    
    * use reader
    
    * continue
    
    * remove variables
    
    reset submodules,fix some errors
    
    reset submodules
    
    revert
    
    * remove s->scene.gps_planner_active
    
    * modify
    
    * cleanup
    
    * done
    
    * resolve conflict
    
    * remove irrelevant comment
    
    * remove white space
    old-commit-hash: d720f28ecb35c06a5ffb6004e79d82c80707fcde

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55e6d11dcd964b27703eed84cc432a5e1b2581731438f78314c493b994051ae5
-size 33939
+oid sha256:7760aae9ec5893dfc59e6a963ec9bb4d625f526ae7122f808dda672460294c68
+size 34208
