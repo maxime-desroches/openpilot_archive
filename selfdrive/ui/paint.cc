1886ccb206f40110b0e998cf7111d2b415773c7b
George Hotz <72895+geohot@users.noreply.github.com>
2020-06-05 17:40:56 -0700

    Getting PC openpilot working again (#1650)
    * fixup keras_runner to work and be nicer
    
    * truncate the lane lines based on the valid len
    old-commit-hash: 7770041f9201c4f84717435b7a585fd70a937923

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7760aae9ec5893dfc59e6a963ec9bb4d625f526ae7122f808dda672460294c68
-size 34208
+oid sha256:cb1ee34eddd60aa875d68a847a16cd6fbd827117f632db467a5cf0633a52f7b4
+size 33995
