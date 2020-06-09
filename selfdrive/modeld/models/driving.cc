81604d23b4cb4d93ab208ee98a8cdc8d3abeeb38
Dean Lee <deanlee3@gmail.com>
2020-06-10 04:46:49 +0800

    add function read_db_bytes (#1662)
    old-commit-hash: 90d97de74ddc4672b35a5186e868794468a44345

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9cfc4371487acc5e018f681a5a617c2493ea354c64ae21c330d529be7c41d52d
-size 11619
+oid sha256:5cf59f11165beeb7edc1cb09016ccdc59a4e3ee6535c5d8dee89bebfbeec0a23
+size 11584
