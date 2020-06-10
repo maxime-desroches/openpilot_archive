a1a21fd68fb861aac001d4134d9d66178b7aeb5c
ZwX1616 <zwx1616@gmail.com>
2020-06-10 15:15:51 -0700

    fix dtype (#1676)
    old-commit-hash: 681ef81ed82650e5cc1d420362d363ee83f7a9ad

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb1ee34eddd60aa875d68a847a16cd6fbd827117f632db467a5cf0633a52f7b4
-size 33995
+oid sha256:0c615c0e4bbcbfd27192e6542369b9c8455f47048c7a187098728fa8ea8f1c93
+size 34003
