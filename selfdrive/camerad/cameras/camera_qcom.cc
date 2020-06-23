db111d077511a7b9c10100a7a5b88e09c8632e21
ZwX1616 <zwx1616@gmail.com>
2020-06-23 14:33:32 -0700

    recover EON/C2 AF (#1665)
    old-commit-hash: 3aa99a01d7a29c69f1b767b3fca30d18a6047425

--- selfdrive/camerad/cameras/camera_qcom.cc
+++ selfdrive/camerad/cameras/camera_qcom.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35eb314745c29f791724412dd13f62c1f7e0e2999983516b1a192bd46d77f4d3
-size 68022
+oid sha256:7a2559eea29fda368bda5445022015c2dc26b2dd0278a1d708ae741f4f7aa9bf
+size 67710
