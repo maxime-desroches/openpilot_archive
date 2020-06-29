e4df66fe84c39d89314511a5e79b598620cfb557
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-06-29 14:12:28 -0700

    remove unused UI variables
    old-commit-hash: e8259895f7d29b153aa2d5e9c86391f47413a133

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eacb4b83da0aabbbe4712db3fa79a76cb3cde2f9d1d667d3bbbcd64d63a267a4
-size 34089
+oid sha256:3d93a0598bd8f58ca1d614d86cbaf26d7e9e00f85fa49d10f83ace41ceeade83
+size 34029
