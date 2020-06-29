f2e8c6655641930ab673c238781b6fe1aa26b4da
George Hotz <72895+geohot@users.noreply.github.com>
2020-06-29 13:54:23 -0700

    UI 10x speedup with hardware antialias (#1787)
    * enable MSAA, disable nvg antialias
    
    * less opaque and clean up
    
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: b62da571b4108487cfc4d2dbd1970a504ba0a317

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b61e956ee748c5cbfa1152a5da086d60e0e4e12db7ab30c32793aace6e8f3e8
-size 34070
+oid sha256:eacb4b83da0aabbbe4712db3fa79a76cb3cde2f9d1d667d3bbbcd64d63a267a4
+size 34089
