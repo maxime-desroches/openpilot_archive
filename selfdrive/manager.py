eeaafad96b50e64c0fef358fcc0851e21d408fc2
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-06-30 14:01:40 -0700

    flippening is only for android
    old-commit-hash: ec8b69cb14fa83c90e9f5388d52ced603613e9bf

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5da703f8ceee5de4434b963ed091e0f84d12bcb9b1d8784864b6dcb133bcaa34
-size 18204
+oid sha256:7890c6c958e534c41cef88d85d4a2a23a4bed4edca6c8f634984802c99c5f746
+size 18226
