ce0342e868542a1bd8ed8be9597e216f363d43aa
Adeeb Shihadeh <8762862+adeebshihadeh@users.noreply.github.com>
2020-07-03 15:40:13 -0700

    Refactor CPU usage test (#1802)
    * pull CPU usage test out of manager
    
    * remove that
    
    * add to release files
    
    * cleanup
    
    * executable
    
    * this should work
    
    * check if car started procs are running
    
    * debug
    
    * add min cpu usage
    
    * remove debug prints
    
    * adjust min cpu threshold
    
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: 6db7fa8c033841c82d1139119aad8a20645449f4

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7890c6c958e534c41cef88d85d4a2a23a4bed4edca6c8f634984802c99c5f746
-size 18226
+oid sha256:9add234a379426a2e88bd62e4be14f003c2da2bb5c071bbac9a079122257a781
+size 17463
