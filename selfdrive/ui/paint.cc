2103a70910667eee3a120eb084930e050d4d57da
Dean Lee <deanlee3@gmail.com>
2020-07-04 10:42:26 +0800

    paint.cc: Simplify drawing line (#1643)
    * simplify drawing line
    
    * remove space
    old-commit-hash: ff4f432f0fd2ca5cf592082914e3a3a7b091a3db

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3d93a0598bd8f58ca1d614d86cbaf26d7e9e00f85fa49d10f83ace41ceeade83
-size 34029
+oid sha256:3c9f2890d958413054fba79f7f63b702b5940be75bdbc58c8d9a7086be1d81b9
+size 33780
