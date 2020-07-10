c6e75322796484d7f113561707b88611395f07fa
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-07-09 20:55:56 -0700

    only check frame age for posenet too
    old-commit-hash: c8f3ff8913ce7e18d755ae8015a27af3365029e9

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ca708b67aae7aaff6961f086c6fbf62076af518aa50c90cc72ce5b7858dd2a21
-size 11958
+oid sha256:5172072764048ea40e37b17f3ff099dd2b79344b121db4be5e74e69db139929a
+size 11938
