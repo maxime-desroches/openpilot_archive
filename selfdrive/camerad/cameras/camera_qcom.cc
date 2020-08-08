fd61da2804b38f8049824ba0f33f5c014d22361b
Dean Lee <deanlee3@gmail.com>
2020-08-09 06:55:09 +0800

    camerad: close ops_sock in camera_close (#1998)
    old-commit-hash: 55ece6991e1fdeb4b109e47c978c4da2a3b23594

--- selfdrive/camerad/cameras/camera_qcom.cc
+++ selfdrive/camerad/cameras/camera_qcom.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82cd9171283779fe5d5d2cb542dd49fe49a2baf8347d5cf1f4da3a00f4294694
-size 69027
+oid sha256:0811e17e87198e86d5a92e754e6fad57e35261291c0f0eeb92d3a0eabafc57a7
+size 69055
