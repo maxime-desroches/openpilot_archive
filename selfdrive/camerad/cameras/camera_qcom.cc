fe5b4421c88304f9efdfc66f62c7a8d691217ac5
Dean Lee <deanlee3@gmail.com>
2020-08-12 02:40:11 +0800

    camerad: protect concurrent access to shared variables and avoid race conditions (#1966)
    * Protect concurrent access for shared resources
    
    * fix
    old-commit-hash: f3fb406eee85b072c727ccaf027bd2dacd3aa69a

--- selfdrive/camerad/cameras/camera_qcom.cc
+++ selfdrive/camerad/cameras/camera_qcom.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0811e17e87198e86d5a92e754e6fad57e35261291c0f0eeb92d3a0eabafc57a7
-size 69055
+oid sha256:264e9739d7c3dd1dca3fdb253d829418ddd59875397ea4a2786f7ebde4db778e
+size 69310
