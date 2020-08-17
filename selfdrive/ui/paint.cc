c0bb891a1d2d34dafcab6e89198792a3ecc3d562
Chris <ChristopherRabotin@users.noreply.github.com>
2020-08-17 03:16:52 -0600

    Kilometers per hour now displayed as km/h (#2032)
    Fixes #2031
    
    Signed-off-by: Christopher Rabotin <christopher.rabotin@gmail.com>
    old-commit-hash: 1b52930a52828b1423c59c677c9b42a5ff76125c

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:97ae02b5a8e9ad37bc0c11a41be4bba2992142c8dd0e972586d7c0e0ea1988c3
-size 33644
+oid sha256:af8d7812dabbaef07426f2a5c180df015cdafd8f8ba397f7b9fc626e1e83f588
+size 33645
