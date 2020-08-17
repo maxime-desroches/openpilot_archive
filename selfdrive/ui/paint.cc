3f7ba9b2756bc1559bbfa7423fe01c68f58c03e4
Dean Lee <deanlee3@gmail.com>
2020-08-17 18:06:09 +0800

    paint.cc: remove redundant calls  (#2025)
    * remove redundant calls
    
    * set MODEL_LANE_PATH_CNT=2
    old-commit-hash: fe43d51686da19063c4ee3ebdb09eadfdd3a56ad

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6992bb12cf0d52afa2bfac5cd4bb7c8e47ac85c630d2367c252b4d3ca908f3d0
-size 33731
+oid sha256:9177d2307af662fd3ba4689c1568f76025306a359b2a3f111b46aa4411e93436
+size 33614
