89a30859de7e934e564ba9f08816c4bf553f7c99
Willem Melching <willem.melching@gmail.com>
2020-08-20 17:16:44 +0200

    Qt ui for PC (#2023)
    * qt ui boilerplate
    
    * this kinda works
    
    * cleanup
    
    * render inside other widget
    
    * cleanup
    
    * more cleanup
    
    * Not needed
    
    * Handle click
    
    * Draw at 20Hz
    
    * create paint.hpp
    
    * move stuff around
    
    * update sidebar
    
    * Draw vision
    
    * this works again
    
    * add clickable settings button
    
    * Only collapse sidebar when started
    
    * always use qt on linux
    
    * fix width
    
    * scrollable area
    
    * talk to NetworkManager
    
    * code to add a connection
    
    * params toggles
    
    * small cleanup
    
    * add qt5 to dockerfile
    
    * Qt on mac
    
    * Add qt to release files
    
    * fix macos build
    
    * nore more ifdefs needed
    
    * add icons
    
    * make a bit nicer
    
    * Hide scrollbar
    
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: e115c514521c7705112bd0cb2e4bf865b536501a

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd5502f2db996323c5109aaed1a8514456f268430fd30b1816311e07bf11acd3
-size 33709
+oid sha256:1cdbceb2d75352089f49c8bda464e84e4f54a3584bbcadeefd942542799c1404
+size 33827
