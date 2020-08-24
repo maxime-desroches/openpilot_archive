28030f012a1da231a7b384986aee292f63c1ee76
Dean Lee <deanlee3@gmail.com>
2020-08-24 20:15:34 +0800

    paint.cc: deleted two meaningless lines (#2043)
    old-commit-hash: 08d6fe18805c3e7e9fe512b1b343ab69d49f12da

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1cdbceb2d75352089f49c8bda464e84e4f54a3584bbcadeefd942542799c1404
-size 33827
+oid sha256:13c113d96a018ce2a1e04a85efd5b7f51b8c1686bf5ac4514645e41a22779352
+size 33726
