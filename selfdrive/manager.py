5a3f6391a07b47d604d211e442472e8f58a6f55b
Willem Melching <willem.melching@gmail.com>
2020-08-26 19:06:56 +0200

    apport support for tombstoned (#2087)
    * apport support for tombstoned
    
    * Update manager
    
    * Update comment
    old-commit-hash: e88edeb051c393d7df84d5cf9b33ad0668bd4e99

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:887bef90816f5c58fc77e498e6f60149064dfb593c543820b059ddc1fd6917d2
+oid sha256:8b68cf91b4185c26a372ffdcfe70781c7feceaf4524c607488fde28c0b9997a0
 size 17978
