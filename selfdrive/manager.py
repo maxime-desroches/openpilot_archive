9396d941ab6175669b4f2d9d7d6fe6090f100118
Willem Melching <willem.melching@gmail.com>
2020-08-26 14:57:17 +0200

    Hardware abstraction class (#2080)
    * hardware abstraction class
    
    * De Morgan
    
    * Rename pc hardware class
    
    * Fix sound card in controlsd
    
    * Pc get sim info
    
    * fix hardware in test
    
    * two more
    
    * No more random imei on android
    
    * no randomness on android
    
    * Need to return something that looks like imei for registration to work
    
    * Return proper network strength
    
    * Unused import
    
    * Bug fixes + gpsd is only android
    old-commit-hash: c7152d54192605c833912b69cf85514ac2e1b1fc

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38355164d9f42c7ba173ef47fc34209a1f8fc8f81cb9df07f3f33c4c0dce4f9c
-size 17856
+oid sha256:2095bef2fed465136e8b484833a8f02042e4369317d573025e7a60b8a98911a5
+size 17934
