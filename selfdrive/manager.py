0a7d564b437550fad334ff9a83422d9d4ce11c7b
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-09-03 16:32:55 -0700

    UI vision refactor (#2115)
    * refactor vision
    
    * don't show slow frame message when in preview mode
    
    * change draws to uint32_t
    
    * set vision_seen=false after destroy
    
    * remove vision_connect_thread
    
    * refactor ui_update
    
    * seelp 30ms when vision is not connected
    
    * remove should_swap
    
    * call ui_update_sizes before ui_draw
    
    * rebase
    
    * start bigger UI refactor
    
    * don't need the touch fd
    
    * fix qt build
    
    * more cleanup
    
    * more responsive
    
    * more refactor
    
    * fix for pc
    
    * poll for frames
    
    * lower CPU usage
    
    * cleanup
    
    * no more zmq
    
    * undo that
    
    * cleanup speed limit
    
    * fix sidebar severity for athena status
    
    * not aarch64
    
    Co-authored-by: deanlee <deanlee3@gmail.com>
    Co-authored-by: Comma Device <device@comma.ai>
    Co-authored-by: Willem Melching <willem.melching@gmail.com>
    old-commit-hash: 848301b091af11e129ba6873f7d297d4890c562b

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3b2fdd535accea5d8075e0c7a68a9df3b09d482190645558594fa81573f713f
-size 17947
+oid sha256:a5b653236291cd15716be0cbc17ba5e0681559299de473a8b7fa57d20c86757b
+size 17913
--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:38f1cd4b79ba6409051d8698fc9da22d23ec704045803839e1c5e6c062660c7a
-size 29812
+oid sha256:af958dea683478e1ff5a11fefc7bbeab9f44d186884e0d183602e5bd6f3277b3
+size 29139
