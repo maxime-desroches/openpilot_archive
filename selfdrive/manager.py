e6847e1f40a55fe3f2d7690733c4cf430037f65b
Vivek Aithal <vivekaithal44@gmail.com>
2020-09-10 03:14:49 -0700

    dockerize carla + openpilot (#2011)
    * dockerize carla + openpilot
    
    * separate dockerfile
    
    * bring back CI dockerfile
    
    * cleanup bridge
    
    * add op docker build and start script
    
    * build container in CI
    
    * fix camerad hack
    
    * remove most magic numbers from bridge.py
    
    * openpilot-sim docker build and run scripts
    
    * fix dmonitoring hacks
    
    * revert controlsd hacks
    
    * clean up build scripts
    
    * singular
    
    * fix path
    
    * fix image name
    
    * modify sim readme
    
    * sim readme and start script changes
    
    * dockerfile with working opengl
    
    * working opengl + passing panda build_st in docker
    
    * fix bug in sim docker file
    
    * bugfix sim docker file
    
    * bugfix all op-sim docker issues
    
    * modify readme + run script
    
    * IT DRIVES
    
    * clean this up
    
    * more cleanup
    
    * cleanup docker stuff
    
    * more cleanup
    
    * start with openpilot-base
    
    * install carla python package
    
    * Script is not in lib
    
    * chmod
    
    * everything should be running in docker now, the code may not be nice though
    
    * works locally...
    
    * rhdChecked is deprecated
    
    * Checkout using git lfs when building sim container
    
    * try to pass the tests
    
    * pull latest docker
    
    * gps should not throw an error on openpilot launch in bridge.py
    
    * fixed a coding style error
    
    * Only start ubloxd in car
    
    * fixed more style problems
    
    * revert typo
    
    * Use enviromental variable to prevent errors in a simulator
    
    * Remove unused import
    
    * Attempt to fix missing enviromental variable
    
    * fix typo
    
    * less work for users, auto tmux engagement
    
    * less work for users, auto tmux engagement
    
    * fix check for nvidia
    
    * clean up nvidia check
    
    * remove typo, shorted dockerfile
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    Co-authored-by: Willem Melching <willem.melching@gmail.com>
    Co-authored-by: Bruce Wayne <batman@workstation-eu-gregor.eu.local>
    Co-authored-by: Gregor Kikelj <gregor1234567890@gmail.com>
    old-commit-hash: c5dfbe7a72c6f31a0aa3a32b6df3f81409b40faf

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5813f9d62b19ceae58f5fcd452f6659d72e43c15470bbcf6852fbd6981c0cdee
-size 17818
+oid sha256:c96f4aaf3b4e48461683b135d5626fffd63d61424abd7d002d92f1751b00acd6
+size 17824
