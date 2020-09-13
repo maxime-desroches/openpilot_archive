50ab884b40b8a39e22de43b11854dcfde7c972c2
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-09-12 17:50:22 -0700

    tici driver monitoring (#2158)
    * use cpu for now on tici
    
    * pin to core 5
    
    * fix driver view
    
    * probably wrong
    
    * correct this time
    
    * RHD too
    
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: fdbfe5292797c5638855fb947ebc5f4eb161353c

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4dce5697034a7326b21a0ba5614e0459005b2fc6984e68caf843e8c65b6b171d
-size 17822
+oid sha256:c8af096413add2066908b1a5a043898772d9d7e14eb7fbaa31a7e01e929ff302
+size 17840
