e57dda08b6949aa2e1c81dcfa94a5a7a233424ed
ZwX1616 <zwx1616@gmail.com>
2020-09-22 14:46:47 -0700

    fix dmonitoringmodeld random high CPU usage (#2216)
    * exp attempt1
    
    * attempt2 less hacky
    old-commit-hash: da147b2534b78f935704b5838cf967593be2affb

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b5e73218c0dfceac43348caad03b9d2962bc1dd61d24def289068da30740bad
-size 17892
+oid sha256:da001810b8f2d3988333fd5e82ada012ef4d181ab5de4c27f9765cf1bd3de6f5
+size 18069
