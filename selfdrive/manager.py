226e8439fb88cbe6254b4b5a90a8396522beb2d3
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-10-07 03:11:54 -0700

    remove green temp processes (#2286)
    old-commit-hash: 87942eea1c7fa6aef9038bae01b3c35261ecff16

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5dc661e1ab7fa160bdd1ead9bd376c814d53802634797e3f4f10fd0239ac16d3
-size 18024
+oid sha256:1395499811c7bb8f3f6c64734d3332acb3b71f74e41023f423f6fd8a8a0447bb
+size 17558
