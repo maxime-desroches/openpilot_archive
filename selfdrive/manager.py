2a9480c7fae86af1a3b89607e865f870726b3f31
Willem Melching <willem.melching@gmail.com>
2020-10-13 16:23:23 +0200

    Params refactor, simplified (#2300)
    * always c++
    
    * Create C++ params class
    
    * get works
    
    * tests hang now
    
    * passes tests
    
    * cleanup string formatting
    
    * handle interrupt in blocking read
    
    * fix memory leak
    
    * remove unused constructor
    
    * Use delete_db_value directly
    
    * Rename put -> write_db_value
    
    * filename cleanup
    
    * no semicolons in cython
    
    * Update common/SConscript
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    
    * add std::string version of delete_db_value
    
    * This is handled
    
    * cleanup encoding
    
    * Add clear method to clear all
    
    * add persistent params
    
    * fix android build
    
    * Should be called clear_all
    
    * only import params when needed
    
    * set params path on manager import
    
    * recusrively create directories
    
    * Fix function order
    
    * cleanup mkdirp
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: 2e182e5c574ea1468678b5fd8d0597e34ed5898d

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1395499811c7bb8f3f6c64734d3332acb3b71f74e41023f423f6fd8a8a0447bb
-size 17558
+oid sha256:f5e062aa165127e24e7c25df15b66cbfaf5aecc7b34d7ed10535c982b57de14b
+size 17555
--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3476aa92010b881eff7a21f9d27e670d052a8c8b2df3b1cfee3514fb7d7c4a57
-size 11251
+oid sha256:237c909997d77920bd270ff3fd8534f3fe074ac36e7a0f9301a42ada0d4aa3e3
+size 11260
