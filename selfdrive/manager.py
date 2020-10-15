e619cfd30a561c7c4dc83f70a5388b46b27352a7
Willem Melching <willem.melching@gmail.com>
2020-10-15 13:15:22 +0200

    Params path only in one place (#2344)
    * fix params paths
    
    * Qcom & qcom2
    
    * This env variable is not used anywhere
    
    * params path in only one place
    
    * fix other PARAMS_PATH references
    
    * absolute path is probably better
    old-commit-hash: 3dd944898183be0ecf985f389d462e53c3feb8e5

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f5e062aa165127e24e7c25df15b66cbfaf5aecc7b34d7ed10535c982b57de14b
-size 17555
+oid sha256:3dbc318d45942aa17ae17896d00fd7d7687420871e3a37a042127511b480768b
+size 17512
