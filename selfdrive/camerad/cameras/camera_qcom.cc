171b99b614cc60c0a4860c98514c10b5693bbc60
ZwX1616 <zwx1616@gmail.com>
2020-10-16 21:58:05 -0700

    Clean up and test camerad refactor (#2310)
    * refactor
    
    add function get_camerabuf_by_type
    
    remove cl_free
    
    use camera_id
    
    move front stuff into frontview_thread
    
    bigger refactor
    
    more
    
    typo
    
    rename camerabuf *c to *b
    
    cleanup header files
    
    remove wideview_thread
    
    camera_qcom.c to c++
    
    fix SConscript
    
    camera_process_buf
    
    space
    
    sendrgb same with before
    
    do autoexposure in processing_thread
    
    correct sendrgb
    
    add camera_common.cc
    
    move yuv_transform to CameraBuf
    
    * use C to keep consitent
    
    * rebase master
    
    * rebase master
    
    * continue refactor
    
    * refactor all camera related data&function out of main
    
     done
    
    * cleanup
    
    * -use buf.rgb_width&height
    
    * remoe ,
    
    * rebase master
    
    * remove unused std::vector
    
    * add func common_camera_process_front
    
    * fix max
    
    * fix sendrgb
    
    * less crap being logged
    
    * fix indents
    
    * add back thumbnails
    
    * thumbnail fixed on tici
    
    * junk junk junk
    
    * unify api
    
    * fix ptr ref
    
    * lol!
    
    * cleanup
    
    * more simplified
    
    * typos
    
    * remove terminate
    
    * use standard polling
    
    * ops_thread refactor, too much code!
    
    * fix build
    
    * ok
    
    * assert
    
    * listen
    
    * incl more
    
    * wq!
    
    * fix more build
    
    * macos
    
    * fix exp targets
    
    * front is different
    
    * const
    
    * bye
    
    * remath
    
    * remove irrelevant
    
    * do ops in a thread
    
    * fix init
    
    * return null
    
    * QCOM2 ops
    
    * qcom2 build
    
    * missing comma
    
    Co-authored-by: deanlee <deanlee3@gmail.com>
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: 6b020241c986f614e9a1f08b5b870ff225cf7b19

--- selfdrive/camerad/cameras/camera_qcom.cc
+++ selfdrive/camerad/cameras/camera_qcom.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ef161d15d96dcfd31969474bee9075923f5216dcf84fb2b4f4ceb4ec02d97e32
-size 69321
+oid sha256:cfa31aa3241e18e9b17b884904fab0560faa1ec956e05c9251147706a092af28
+size 74274
