ec69fb218defacd01403f7734c3a9ca0c9f603ad
Willem Melching <willem.melching@gmail.com>
2020-10-20 12:14:19 +0200

    UI: get video aspect ratio from camera intrinsics (#2351)
    * UI: get video aspect ratio from camera intrinsics
    
    * cleanup video rect definition
    
    * const zoom
    
    * Match zoom level
    
    * Fix comment
    
    * Zoom out more
    old-commit-hash: 9d0b89394429e6ee9f8f9c2849dd19b11fb9b0ce

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e6990696a5b6b1ca5e244572f9f098e57e3b9aa8acf95e63a3b3b9242dc7aab
-size 29029
+oid sha256:5da6b5a4be9b7a8dfd7da407e5a9ba50731b5057e8f5dd881280d8a5e7f266ee
+size 29049
