2e52c8d2dad2c3f55e31c1639be596f8a42869ac
George Hotz <72895+geohot@users.noreply.github.com>
2020-11-03 07:45:16 -0800

    Focus Actuator Comments (#2478)
    * comments from DW9800W datasheet
    
    * more comments
    old-commit-hash: 4d5efb57897832373c2af1435c6d953e9212f814

--- selfdrive/camerad/cameras/camera_qcom.cc
+++ selfdrive/camerad/cameras/camera_qcom.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:258a0700bd269b126bb80f9b7bca9fffae8c01044a1d4795169ce45cb1206edc
-size 74349
+oid sha256:01d602e1d69c2ba5e1c73cdd4b1a708cab443938ef5bd261291758a3b397560f
+size 74671
