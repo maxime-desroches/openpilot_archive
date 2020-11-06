f87158106d73b0c394e984941052a65f961d45d9
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-11-06 12:05:24 -0800

    Qt text window (#2489)
    * qt text window
    
    * auto size
    
    * real text
    
    * this is cleaner
    
    * fix android build
    
    * exit on pc
    
    * tici fixes
    old-commit-hash: 68ba1649b91984c106491cf1894c3f44f21a34df

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:03f08e2baf7362fbebdd39d37bcd69cb5cf21b6c6a0651e17891ad1f39f98b4e
-size 17558
+oid sha256:f86840cf6918f65b99a54e748c9a941e2bd5b9e53451580712a8328e2fe309e9
+size 17546
