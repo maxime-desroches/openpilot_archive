67afa38064b9d7d582852a95d4c18a537467eb2f
HaraldSchafer <harald.the.engineer@gmail.com>
2020-11-11 20:31:46 -0800

    Torch model (#2452)
    * refactor draw model
    
    * rebase master
    
    * correct valid_len
    
    * rename function
    
    * rename variables
    
    * white space
    
    * rebase to master
    
    * e16c13ac-927d-455e-ae0a-81b482a2c787
    
    * start rewriting
    
    * save proress
    
    * compiles!
    
    * oops
    
    * many fixes
    
    * seems to work
    
    * fix desires
    
    * finally cleaned
    
    * wrong std for ll
    
    * dont pulse none
    
    * compiles!
    
    * ready to test
    
    * WIP does not compile
    
    * compiles
    
    * various fixes
    
    * does something!
    
    * full 3d
    
    * not needed
    
    * draw up to 100m
    
    * fix segfault
    
    * wrong sign
    
    * fix flicker
    
    * add road edges
    
    * finish v2 packet
    
    * Added pytorch supercombo
    
    * fix rebase
    
    * no more keras
    
    * Hacky solution to the NCHW/NHWC incompatibility between SNPE and our frame data
    
    * dont break dmonitoringd, final model 229e3ce1-7259-412b-85e6-cc646d70f1d8/430
    
    * fix hack
    
    * Revert "fix hack"
    
    This reverts commit 5550fc01a7881d065a5eddbbb42dac55ef7ec36c.
    
    * Removed axis permutation hack
    
    * Folded padding layers into conv layers
    
    * Removed the last pad layer from the dlc
    
    * Revert "Removed the last pad layer from the dlc"
    
    This reverts commit b85f24b9e1d04abf64e85901a7ff49e00d82020a.
    
    * Revert "Folded padding layers into conv layers"
    
    This reverts commit b8d1773e4e76dea481acebbfad6a6235fbb58463.
    
    * vision model: 5034ac8b-5703-4a49-948b-11c064d10880/780  temporal model: 229e3ce1-7259-412b-85e6-cc646d70f1d8/430  with permute + pool opt
    
    * fix ui drawing with clips
    
    * ./compile_torch.py 5034ac8b-5703-4a49-948b-11c064d10880/780 dfcd2375-81d8-49df-95bf-1d2d6ad86010/450 with variable history length
    
    * std::clamp
    
    * not sure how this compiled before
    
    * 2895ace6-a296-47ac-86e6-17ea800a74e5/550
    
    * db090195-8810-42de-ab38-bb835d775d87/601
    
    * 5m is very little
    
    * onnx runner
    
    * add onnxruntime to pipfile
    
    * run in real time without using the whole CPU
    
    * bump cereal;
    
    * add stds
    
    * set road edge opacity based on stddev
    
    * don't access the model packet in paint
    
    * convert mat.h to a c++ header file (#2499)
    
    * update tests
    
    * safety first
    
    Co-authored-by: deanlee <deanlee3@gmail.com>
    Co-authored-by: mitchell <mitchell@comma.ai>
    Co-authored-by: Comma Device <device@comma.ai>
    Co-authored-by: George Hotz <george@comma.ai>
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    old-commit-hash: 08846b5c0ec3a5299c8cb73904da15a18d3aaccf

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:237c909997d77920bd270ff3fd8534f3fe074ac36e7a0f9301a42ada0d4aa3e3
-size 11260
+oid sha256:616b45ddad1e3abdd6b7cefae66e66dc7bc63bb6864e93ecc4ccbc5ea2fc6203
+size 18483
--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4fd3c5007ee1f9404201b92f5a1211af7af11db8c95cdd193e4c95ba5f8685a
-size 29092
+oid sha256:c74632d699888145e7519b81f3681dc7ee8a956de51b1ec30a22bb26c6c00472
+size 26599
