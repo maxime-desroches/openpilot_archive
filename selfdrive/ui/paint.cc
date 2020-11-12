a0e688dcd4b742b33ae2c95df5677f7eb707d5d9
Dean Lee <deanlee3@gmail.com>
2020-11-13 03:30:04 +0800

    remove std::clamp (#2530)
    old-commit-hash: 91f79acac3ef63d28d5884bf2947eb7b692666cc

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c74632d699888145e7519b81f3681dc7ee8a956de51b1ec30a22bb26c6c00472
-size 26599
+oid sha256:2796a5a7a50705ef40c84d15f70fb9b5d6a4ebeb4c33ba6a472706c70fe0709c
+size 26517
