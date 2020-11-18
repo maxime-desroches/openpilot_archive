32fcc980be0fe392c9039ab477a324765b145777
George Hotz <george@comma.ai>
2020-11-18 09:36:53 -0800

    clear both possible places for the scons cache
    old-commit-hash: 599f000cde3f94ca0843770ef9889cbcc5173398

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e59111d7fb371dfe4a4f6956872b38e289db2f1dbf0009a6994061df71a426bf
-size 17448
+oid sha256:807f771f746c4366e44a6456db7813c04df21d91919a131a91d0dbad587efd9c
+size 17533
