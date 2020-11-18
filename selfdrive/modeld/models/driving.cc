be879c0ba6973e01d9e7a64a85b9b89e76e2d8c1
HaraldSchafer <harald.the.engineer@gmail.com>
2020-11-17 20:04:32 -0800

    Minor fixes 08 (#2565)
    * correct indexes
    
    * best metrics yet 30231f46-08af-477e-9d30-776593913c24/700
    
    * sometimes x decreases
    
    * update model refs
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    old-commit-hash: 7b216e7cbb30f9a7b164cc17bb66b348d728b65e

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:616b45ddad1e3abdd6b7cefae66e66dc7bc63bb6864e93ecc4ccbc5ea2fc6203
-size 18483
+oid sha256:1da09c0c2a7e9c604b154d6515694287aaf449cd10996cbfbba32643d9d35ac2
+size 18371
