7e8c4e9f124e22d08ac9e5e89b6d2d309b61ee71
Willem Melching <willem.melching@gmail.com>
2020-11-20 13:18:33 +0100

    use HARDWARE.reboot() when unkillable process doesnt stop
    old-commit-hash: 3289de0aa389615dfbb73de1c921ae665088eee6

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:807f771f746c4366e44a6456db7813c04df21d91919a131a91d0dbad587efd9c
-size 17533
+oid sha256:58c4c8ab62ca93b25676b8fca9a68b7a584315200f05b1fd1335b6156d47b9d7
+size 17406
