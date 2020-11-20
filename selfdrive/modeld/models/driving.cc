a66a6c2d988836921c5a1acb11e4a3c60869aab6
HaraldSchafer <harald.the.engineer@gmail.com>
2020-11-19 21:05:29 -0800

    fix frame drop percentage (#2578)
    * correct, I think
    
    * add model execution time
    
    * remove unused
    
    * token is a file
    
    * this is gonna change
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    old-commit-hash: 7378bc42845a094bbb22fb76154cb40ecb098aaf

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1da09c0c2a7e9c604b154d6515694287aaf449cd10996cbfbba32643d9d35ac2
-size 18371
+oid sha256:a66c84fc00d70349f6e7dc95509ff7284eae520fe0d280ca7d70ba6940d3c062
+size 18615
