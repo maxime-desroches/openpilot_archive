ed95f6523fc176ace92a75a0b29e182f2a7a6380
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-11-21 00:33:01 -0800

    no spinner if not main
    old-commit-hash: cb9bf8df23aa537e40ae3b85d5a34c16bfda2dce

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58c4c8ab62ca93b25676b8fca9a68b7a584315200f05b1fd1335b6156d47b9d7
-size 17406
+oid sha256:ccece8635c6a3819462097a665d3806738dff70f61e5b41a2bf9349e1848714a
+size 17451
