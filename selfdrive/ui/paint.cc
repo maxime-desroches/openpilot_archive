cf0e5af41d43b8cae8bfec4d60f5d60637a1e0ca
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-11-27 14:25:52 -0800

    no gl inside nvg frame
    old-commit-hash: b9c14c92a7603e2be08cb519cedfb19e563caeb9

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ea2ad49729e254aecf4088e247ed0ff70fab162e5eb1d7d4eb0c65c90dee54f
-size 26521
+oid sha256:ea9bf4dae031bf4a658b8a523813c88477707ff54d8ce40737a7073354e9618c
+size 26808
