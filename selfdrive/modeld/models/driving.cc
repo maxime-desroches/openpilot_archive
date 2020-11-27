e8b8626d7766e4141ba521b50fa160e0dbb57a7e
Willem Melching <willem.melching@gmail.com>
2020-11-27 16:13:37 +0100

    remove check on frame drop in modeld (#2638)
    old-commit-hash: feaecbd615a13a93b9486aeb6777c854ae0b0e6a

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:906fb95a08b82c4a846556c5969c4d22b53244e9d2b44a0405ee57b41d8826bb
-size 18613
+oid sha256:883d725c3a88d3b45c33aa8c0ddeb275bd61d128ea7211811941f6a9b3c8c97b
+size 18556
