819358af99f023419f871211a24d2e2a2a220d77
Dean Lee <deanlee3@gmail.com>
2020-12-08 21:20:06 +0800

    small cleanup (#2715)
    old-commit-hash: 232cb25683ef59a4cc1ce6a7a98eeb57a78a9968

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:618a9d128bc28fab86cbb4967372a759a62ddcb54f36ed6fc87a2010e10ed2a1
-size 26900
+oid sha256:126a3bbdfa0528bd198bb0a0418b29a122b3e3e68b24b8552ae31cb5437a26d3
+size 26789
