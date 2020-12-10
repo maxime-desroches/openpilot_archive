522757bbfb8c633f1eb7bd9cfa72246f614dd688
HaraldSchafer <harald.the.engineer@gmail.com>
2020-12-09 22:48:14 -0800

    Test model refactor (#2720)
    * template funcion fill_meta
    
    * add function get_plan_max_idx
    
    * add function get_mdn_max_idx
    
    * remove temp builder variables
    
    * get_mdn_max_idx->get_lead_data
    
    * get_pan_max_idx->get_plan_data
    
    * fill_lane_line
    
    * combine fill_lane_line&fill_path into one function
    
    * fill_lead_vw
    
    * using unique_ptr
    
    * prefer using constexpr
    
    * more constexpr,remove duplicate defined TRAJECTORY_SIZE
    
    * remove suffix _arr from variable name
    
    * misc
    
    * remove extern c
    
    * refactor model_publish
    
    * remove unused paramaters
    
    * traffic_convention to c style array
    
    * c style array:prev_desire&pulse_desire
    
    * fix error&make easy for review
    
    * const mat3 &transform
    
    * move cl_command_queue into ModelState
    
    * use maco LEAD_MHP_SELECTION
    
    * move constexpr from .h to .cc
    
    * remove #define MODEL_NAME
    
    * modeldata.h: contexpr
    
    * remove param temporal from model_init
    
    * helper function get_best_data
    
    * fix probs
    
    * int
    
    Co-authored-by: deanlee <deanlee3@gmail.com>
    old-commit-hash: 630c7309a560e573a9398acd77c96aedcb65dfae

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:08f4d8b4b40115710367791603c41fe551773ad390e4ef31da640fef91d1b289
-size 17728
+oid sha256:64d95ca4319a30ffe8d9fcc391b7e5cfb91c5147ce8ffafdb259b9ddc7a95f0f
+size 15297
