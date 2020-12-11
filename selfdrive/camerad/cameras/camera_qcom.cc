1a3c91726b14791ef29ad69ef1d06b770de9c262
Dean Lee <deanlee3@gmail.com>
2020-12-11 20:05:32 +0800

    clutil refactor (#2733)
    * cleaup clutil.h
    
    * clutil.c -> clutil.cc
    
    * std::hash
    
    * try simplify functions
    
    * using namespace group help functions
    
    * continue
    
    * add function get_platform_info
    
    * continue
    
    * remove caching
    
    * struct CLContext
    
    * std::cout & CL_ERR_TO_STR
    
    * remove CLU_NO_CACHE
    
    * Revert "struct CLContext"
    
    This reverts commit 882e413f227e4c1ba4bb83f956cc67739a189221.
    
    * cl_get_error_string : remove undefied error codes
    
    * replace all printf with std::cout
    
    * util::read_file
    
    * cleanup
    
    * revert CL_ERR_TO_STR
    
    * simplify
    
    * add macro CL_ERR_TO_STR
    
    * cl_get_error_string: fix undeclared errors
    
    * log_size should not +1
    
    * use std::stirng log
    
    * apply review
    old-commit-hash: 3c08d0f1f7e7c9f376fd69fd46067c6f6ff59d39

--- selfdrive/camerad/cameras/camera_qcom.cc
+++ selfdrive/camerad/cameras/camera_qcom.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d087cd2378150053cad7af92ed654a6a6f65e1c86c50438b020f777d597a1c8c
-size 74762
+oid sha256:57b1545299b7ba14108aab2b66f5f81b2a1f55da478fe14b041623742e2cb936
+size 74764
