de11259b3da58f938c20d47e9ffb688eaeff2a93
Dean Lee <deanlee3@gmail.com>
2020-12-15 07:33:40 +0800

    refactor imgproc/utils (#2766)
    * refactor imgproc/utils
    
    * const
    
    * space
    
    * return value
    old-commit-hash: 554ea8f54a699950a9226e1d0808cc41ca4859f0

--- selfdrive/camerad/cameras/camera_qcom.cc
+++ selfdrive/camerad/cameras/camera_qcom.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57b1545299b7ba14108aab2b66f5f81b2a1f55da478fe14b041623742e2cb936
-size 74764
+oid sha256:2679afca25ca3000dba9ce1973e4dbdcc69664818add5c02368e19947bc94924
+size 74786
