f4967556f14b38476f38066208d05bfc21ddd98d
Willem Melching <willem.melching@gmail.com>
2020-12-15 14:47:53 +0100

    paint.cc: lower y offset
    old-commit-hash: 93cc7335fc1ae6035d31a18425ada815cad8a256

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30b45086baf0ee61833a314db672a1e91d6f7e5f9e1366fdac02aef74d072be3
+oid sha256:f32f2b50a04a8255e65a6e7b4559ac81c6c8ef2f261ba8a1b88d84327b25ba38
 size 26933
