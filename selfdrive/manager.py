b830f72d558326dcb0385bb8c0f5b8967746db3e
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-12-16 21:30:23 -0800

    HW abstraction layer (#19530)
    * start hw refactor
    
    * move that
    
    * pins
    
    * put that back
    old-commit-hash: 8674b023bab2b44405139ceec5785a389f837682

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f10b908d54ed26a66c8fece00faa336cd1231455838f6a4d25dbe998fdfc996
-size 17281
+oid sha256:a19af5e41d27f1a9d7b561bc7efe4c5a17a3cba940bdb57e9fe6ea5cac15d0a1
+size 17032
