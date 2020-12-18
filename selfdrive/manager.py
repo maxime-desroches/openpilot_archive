9dc011f9a9f2a5433f063324ffb9cb0e2f58dfad
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-12-18 14:47:25 -0800

    fix startup spinner for non-C2 (#19536)
    * limit build progress
    
    * cleanup
    
    * types
    
    * comment
    old-commit-hash: 71d317872a8df87e571a9078d874e776400df930

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8802f91945e7fb8d899c94950d89fce3ada34da05855588ff118ab17381a30e0
-size 16605
+oid sha256:f2bd8c8c54b798bcfb6a3ea2ffe8cf1c0df316f8ee7fd01da4707b4504e874d7
+size 16599
