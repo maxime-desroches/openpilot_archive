a714e087fb34f652162541ce5f857867122da672
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-12-18 16:40:23 -0800

    offroad home styling and cleanup
    old-commit-hash: a36632cc392151165af62e443386489202ef10ae

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24a3fb5408561ed5c374ae78aa9f3847dc448f91af28191ca196ca41f0774ae6
-size 26922
+oid sha256:bb5f66959164339cf94cdfa8734b683a55303c525acd9b9f320bf0b4f97242ad
+size 26921
