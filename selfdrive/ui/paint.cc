b094ac7fe2d6322ccba884b29917e392e97a4c4e
Mayfield <3902405+mayfieldiv@users.noreply.github.com>
2020-12-22 02:34:24 -0500

    add start_time parameter to unlogger (#19573)
    * add start_time parameter to unlogger
    
    * fix ui crash
    
    * fix ui build
    old-commit-hash: 5864f566df948e073ebe263eb3893ade2f566037

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb5f66959164339cf94cdfa8734b683a55303c525acd9b9f320bf0b4f97242ad
+oid sha256:dd3f458945f2261b90441f4a861780090b891d8f084fffa53b2f9de3a4cad1eb
 size 26921
