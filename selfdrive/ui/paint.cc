bb78116a539293f3ce8276a2ef930d499f035485
Dean Lee <deanlee3@gmail.com>
2020-12-30 06:08:49 +0800

    ui_draw_chevron: simplify calc size (#19616)
    old-commit-hash: 39bd8c7ce6e7e7f63fd4873cabf164e4c028009c

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd3f458945f2261b90441f4a861780090b891d8f084fffa53b2f9de3a4cad1eb
-size 26921
+oid sha256:15b2f11f70385dd170492042e68f24292da6e79730a556de0603ea66aac3cf66
+size 26898
