0814a58912c9b4570dc6fb9a35d3fbb7a24dcfcc
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-12-29 22:32:03 -0800

    manager tests + make all processes exit cleanly (#19595)
    * manager tests
    
    * logcatd exits cleanly
    
    * sigint
    
    * boardd
    
    * multiple dbus connections hangs for some reason
    
    * clocksd proclogd
    
    * network type from thermal
    
    * fix tests
    
    * fix android logcatd
    
    * fix mac
    
    * fix mac proclogd
    
    * move on device athena tests
    
    * build first
    
    * build first
    
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: ffa7e0cbdb0859751685caae1321d3ccf732fed9

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2bd8c8c54b798bcfb6a3ea2ffe8cf1c0df316f8ee7fd01da4707b4504e874d7
-size 16599
+oid sha256:64956252be225f601fbf2f46eb537ed4b919d9daa2d1606482119866154f7595
+size 16559
