c63110f2866dd0405aeaf443ff54631fdda6a1e9
Dean Lee <deanlee3@gmail.com>
2021-01-04 22:55:04 +0800

    simplify ui_draw_vision_maxspeed (#19642)
    if is_curise_set
    old-commit-hash: f232f86a73dd635c2601f8da83a494100d2c3039

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5efad68467a8acac6bd44c23544af3aa875a6722b738d39744ca9fb4fd7bd1a5
-size 26629
+oid sha256:6ef1ca1317278a8ae351e1ba59e6c5ebe6515c6b7f1c937fd2940c1c3991ca6f
+size 26229
