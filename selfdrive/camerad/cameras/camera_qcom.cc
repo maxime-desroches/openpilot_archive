b84bca70bd561a340e11cf9e6880d7d4203270d8
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-01-05 12:50:23 -0800

    setup clang tidy (#19533)
    * setup clang tidy
    
    * config
    
    * some cleanup
    
    * little more
    
    * remove that
    
    * fix qcom build
    old-commit-hash: 87950eb3127e175f0f2b0bb42440f5350c679369

--- selfdrive/camerad/cameras/camera_qcom.cc
+++ selfdrive/camerad/cameras/camera_qcom.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f37da4955aae93a3ab9f69c0718919b803a6ce211cd23d834def86cfec34c673
-size 73618
+oid sha256:2d9c6d012b97f7e0d380cccebf56df77da93fbc400cdbbb44fab29d62b1962c5
+size 73569
