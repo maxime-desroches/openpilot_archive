f209983e05c4b9ac25622102da7c0624b8ab4cfa
Willem Melching <willem.melching@gmail.com>
2021-01-06 11:34:52 +0100

    UI: refactor model related functions (#19657)
    Squashed commit of the following:
    
    commit 7d7fb15e4cdd0e47fa38f949f79581dc611db015
    Author: Willem Melching <willem.melching@gmail.com>
    Date:   Wed Jan 6 11:31:09 2021 +0100
    
        no extern
    
    commit 901893966e3f908435bceb8777253dfa916a3d91
    Merge: 9816e01e c0703a57
    Author: Willem Melching <willem.melching@gmail.com>
    Date:   Wed Jan 6 11:27:29 2021 +0100
    
        Merge branch 'master' into refactor_model_draw
    
    commit 9816e01e6ff0284521fa18b07ec15b3900577687
    Author: deanlee <deanlee3@gmail.com>
    Date:   Wed Jan 6 04:02:08 2021 +0000
    
        fix lead_d
    
    commit 6fa500dbce26544cc3fdeb4d9b0d2d654382a40b
    Author: deanlee <deanlee3@gmail.com>
    Date:   Tue Jan 5 13:18:20 2021 +0000
    
        populate model data in update_socket
    
    commit e25db7e28c1b543a04c3e837aa44431c42dbb5b2
    Author: deanlee <deanlee3@gmail.com>
    Date:   Tue Jan 5 12:59:13 2021 +0000
    
        update model in function
    
    commit 0834a3ffe7eaa080671f13c32dd6475ae5392d5b
    Author: deanlee <deanlee3@gmail.com>
    Date:   Tue Jan 5 12:50:21 2021 +0000
    
        continue
    
    commit 95b08679c2593eb0770a68b3cf1a59f1fb3b74f4
    Author: Willem Melching <willem.melching@gmail.com>
    Date:   Tue Jan 5 13:26:34 2021 +0100
    
        Update selfdrive/ui/paint.cc
    
    commit 0eef065ae4f96c2ee5d97b4ccdfdbf0ae4945385
    Author: Dean Lee <deanlee3@gmail.com>
    Date:   Tue Jan 5 20:11:01 2021 +0800
    
        Update selfdrive/ui/paint.cc
    
        Co-authored-by: Willem Melching <willem.melching@gmail.com>
    
    commit 955ad9b03a176174b86b28d5bb6f48d6e2a09281
    Author: deanlee <deanlee3@gmail.com>
    Date:   Thu Dec 31 04:45:31 2020 +0000
    
        refactor model related functions
    
    old-commit-hash: 37cb4bec631197f6b369bf89f94786772bd05336

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ff06d32e999e9076366b449d85b5701c7d1b2872c9d5487c757e45584809f02c
-size 26151
+oid sha256:fe619b3ab5c05e2a9abf77c973fbdc7d1101ea2994dc3dc0b13bb25a64499122
+size 23891
