5e6628421148da8f55b797ded8e62a22d153464c
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-01-06 12:10:38 -0800

    only run logcatd while onroad
    old-commit-hash: 94b2818b22a13457d59e7c463516e006c5995576

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a12bde89fec303d131b316535e4da94b99884876c3b409c21e281554a3b80c46
-size 16654
+oid sha256:d7f40efb1e9dfe077b2a7eb4582a088d9ad8aabb5474b36115c9255ad8c04244
+size 16652
