5d6f685225dc1641f68f78624c177bf776baf731
ffhspa <31392294+ffhspa@users.noreply.github.com>
2021-01-07 11:15:59 +0100

    remove double semicolon in paint.cc (#19684)
    old-commit-hash: dd9d8bba89c59164738e840609540ffc951d55e9

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e2752954aa4d82aadfe262658530e884329381a2534fa5ee9cb4b5cb1d06dfe4
-size 23895
+oid sha256:f2e72e472e782a1fc6339fd5e7b97bec9ae09f96596a2bb27fe6cec298930c08
+size 23894
