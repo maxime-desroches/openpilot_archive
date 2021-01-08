bac763fa32aec2de51594e4bc19f834bc7404e8a
Dean Lee <deanlee3@gmail.com>
2021-01-09 06:31:21 +0800

    create_thumbnail in camera_common (#19553)
    old-commit-hash: cefdca2fd67ed7cef1e536504ac71e631b7608ab

--- selfdrive/camerad/cameras/camera_qcom.cc
+++ selfdrive/camerad/cameras/camera_qcom.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4aa8f62361be4e28209d98f23ce44f80e5e237332263820cb9a2dbe49cb96429
-size 66038
+oid sha256:455354b78fcbe16fa05bdd89206d2a2f1d2866699e326eb33f84ececfffbaf21
+size 65948
