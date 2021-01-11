f09ae85f4a12a77810608bee28523b152481df0e
Dean Lee <deanlee3@gmail.com>
2021-01-11 22:52:58 +0800

    Refactor image texture stuff into class (#19719)
    * Refactor image texture stuff into class
    
    * cleanup
    
    * Update selfdrive/common/visionimg.cc
    
    Co-authored-by: Willem Melching <willem.melching@gmail.com>
    old-commit-hash: b3107e153cca4aa98b623b80fdd13b8446f76d1c

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5cb45d22458e87fb073ea522d2a42863f8975fafb2a2188bbfdfa2dece89208b
-size 23375
+oid sha256:05945fe547e7a4f819946f7338243876f247d497d8e5913f9ee2d7ca96bbacfb
+size 23276
