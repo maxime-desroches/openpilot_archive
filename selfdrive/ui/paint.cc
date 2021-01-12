a6c4b1bd988088d690d12f88224ca258c6ebee93
Dean Lee <deanlee3@gmail.com>
2021-01-12 22:43:34 +0800

    convert glutil.c to gltuil.cc & add class GLShader (#19720)
    * c++ glutil
    
    * function getUniformLocation
    
    * rebase master
    old-commit-hash: cef469cbed1d835e70e952d06e1fd8b4eabca450

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05945fe547e7a4f819946f7338243876f247d497d8e5913f9ee2d7ca96bbacfb
-size 23276
+oid sha256:818f3a7d0ae95096a381e5c2db8605c6d50a9ff699e5f96ad568ec685e04c195
+size 23103
