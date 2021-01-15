947665fcdf954cd2687af4d6fe292995bb3e9328
Dean Lee <deanlee3@gmail.com>
2021-01-15 18:10:52 +0800

    camerad use std::thread (#19771)
    old-commit-hash: 1b3c93813d37e09b0c695a9974cfc8789373352c

--- selfdrive/camerad/cameras/camera_qcom.cc
+++ selfdrive/camerad/cameras/camera_qcom.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6bd986fa4d14dec86792f75b199489475b581f34d2120306f1132e69801b4a1
-size 65903
+oid sha256:a2434969ed97ca4f958acdca7d1c187a0adfce3290f0153d655f0595521a7cfd
+size 65708
