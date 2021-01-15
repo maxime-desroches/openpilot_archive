9e9af5e3a468c57281a95043a9e66280721b7ea3
Dean Lee <deanlee3@gmail.com>
2021-01-15 18:12:56 +0800

    fill_frame_image: reduce function parameters (#19736)
    * reduce function paramaters
    
    * add assert
    
    * Trigger Build
    old-commit-hash: ea9835f293e9c59715f794cd82474469965c1710

--- selfdrive/camerad/cameras/camera_qcom.cc
+++ selfdrive/camerad/cameras/camera_qcom.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2434969ed97ca4f958acdca7d1c187a0adfce3290f0153d655f0595521a7cfd
-size 65708
+oid sha256:520e5fd6517ea9690528d2e07dc26b9fe90b82d4ca79c5f5ef22fb263789f512
+size 65634
