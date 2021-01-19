d15372ce19d95687cf79a7ab06e3b3e42ae05558
ZwX1616 <zwx1616@gmail.com>
2021-01-19 15:11:15 -0800

    face icon reflects DM policy  (#19842)
    * send that
    
    * draw icon based on whether active
    
    * Update selfdrive/monitoring/dmonitoringd.py
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    old-commit-hash: 88430f12f426ada4691de93e0193ab1873df4eb4

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06c3cf2ede8b0de535a1cebf9d7979fc6b7d228ddd61211991fa4cd777850502
-size 23506
+oid sha256:fb503c17e0ba9bbf6add4ee2e04c8fdd1098ac23556ae8036fb89ec5ca70f0fd
+size 23537
