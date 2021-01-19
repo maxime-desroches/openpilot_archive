220ffdea8a9814aac07d1613ac3f2c3ec71bc536
Dean Lee <deanlee3@gmail.com>
2021-01-20 04:16:12 +0800

    loggerd: split bootlog to a separate program (#19831)
    * bootlog
    
    * blank lines
    
    * move file_exists to util.h
    
    * src = ['loggerd.cc']
    
    * log_init_data
    
    * Revert "src = ['loggerd.cc']"
    
    This reverts commit 18a587023d75b3a3a54f1ceaf9cb31a51640a067.
    
    * add bootlog to .gitignore
    
    * use s->has_qlog instead of true
    
    * add blank lines & remove extern C
    old-commit-hash: f973c56a3695525442ac6c3e89c644b27cbeb384

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7f40efb1e9dfe077b2a7eb4582a088d9ad8aabb5474b36115c9255ad8c04244
-size 16652
+oid sha256:b8333b3bf027a2c75cb8ddfb98434b6a9923b585973185df79908ce2df384de5
+size 16637
