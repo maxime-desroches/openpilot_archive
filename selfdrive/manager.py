40dc2bb5cfc79c8fcaf8d843226fc46183eb8b5d
Willem Melching <willem.melching@gmail.com>
2021-01-20 14:30:59 +0100

    fix /dev/shm permisisons, fixes offroad
    old-commit-hash: 3a13c45c7f4efb6631bd9479be8c59395971f841

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c1548d60a8df7d36579a11d9e2cc6209945292097279ea3d9790171811c4ef83
-size 16520
+oid sha256:a8faa61bf3df028ea20a36496df86ac45fe6fd950ff0af0ca369d9cf1936b202
+size 16589
