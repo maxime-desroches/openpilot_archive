6612146cb3860c70f73161cf8e0acef635c1e2a3
HaraldSchafer <harald.the.engineer@gmail.com>
2021-01-19 17:15:16 -0800

    remove old model packet (#19769)
    * remove publisher
    
    * move to V2
    
    * radard new model
    
    * fix plant
    
    * change packety
    
    * need hack here too
    
    * change to new
    
    * this has been wrong all along
    
    * no more model msg
    
    * subscribe to new model
    
    * not needed anymore
    
    * make work
    
    * need to ignore that too
    
    * should pass tests, needs car test
    
    * fix process replay
    
    * no more poly
    
    * update refs
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    old-commit-hash: 124100d0fa42485edaf86cb25f77f27e8917eed1

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b26f4782a39fce05d55c4ffcbabe2555008706a0bfe50999c6b08af5f7dcc1bf
-size 15184
+oid sha256:f6c6100c20ad8ee538a37061ad00190042eacc4a7c6a696699b08ca5a52f6acc
+size 11057
