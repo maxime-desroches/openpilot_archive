ba9823faf008c79c17af01bafdc9adffc634b6fc
George Hotz <72895+geohot@users.noreply.github.com>
2021-01-19 18:08:31 -0800

    Thneed load/save (#19700)
    * start thneed load/save
    
    * compiling
    
    * fix loading
    
    * build thneed model in scons
    
    * don't hardcode /data/openpilot
    
    * release files
    
    * those too
    
    * support for loading/saving binary kernels
    
    * save binaries out of json band
    
    * make binary a command line flag to the compiler
    
    * need include assert
    
    * fix shadowed common in SConscript
    
    * cleanup run.h
    
    * hmm, the recurrent buffer wasn't 0ed
    
    * ugh, unique ptr
    
    * remove power constraint, refactor record
    
    * Revert "remove power constraint, refactor record"
    
    This reverts commit bb6fa52db6df59cd9d6420a6f630430e35af8a5e.
    
    * print on thneed stop
    
    * fingers crossed for this one
    
    * recorded
    
    * just curious
    
    * okay okay, pass tests?
    
    * cleanups
    
    * refactor wait
    
    Co-authored-by: Comma Device <device@comma.ai>
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    old-commit-hash: 59fac9fdc630fa78f520fc9a244f9f9cd39fa562

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f6c6100c20ad8ee538a37061ad00190042eacc4a7c6a696699b08ca5a52f6acc
-size 11057
+oid sha256:3241d718f94caa48d558307e8d13eb21b4629efded89847afb764814637f9cd4
+size 11281
