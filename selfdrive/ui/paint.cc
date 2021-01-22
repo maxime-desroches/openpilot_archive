df8fc3cc5e6122b3562f2158b2bccf432bfca381
Dean Lee <deanlee3@gmail.com>
2021-01-23 04:16:45 +0800

    UI: cleanup draw_alerts condition (#19875)
    old-commit-hash: 8b4731ffc89dbaf06964620f3b8623e24b920f55

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb503c17e0ba9bbf6add4ee2e04c8fdd1098ac23556ae8036fb89ec5ca70f0fd
-size 23537
+oid sha256:d92228c026177cdc5c7a086c39ec60bbb80ed238f7ecd1dc8e8476099ad8f338
+size 23479
