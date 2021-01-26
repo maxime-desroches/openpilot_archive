06241577cc1a755e84a6e8f33966b5c2de65d0a6
Dean Lee <deanlee3@gmail.com>
2021-01-26 23:56:37 +0800

    model_init: remove memset (#19921)
    old-commit-hash: ccf055fd6267bd6bfa0189d2f61d39574548fa80

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ccf8eedeebb407fcadc75753f22b24e60f38367c24da63c87c983ef3049f0db3
-size 11279
+oid sha256:0d279e197af25a39a65fa13430a717380b76a1cd85331b0509dde56b8d3c14c1
+size 11224
