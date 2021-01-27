aa2752fc174e868513286aae85ba2d1b388e61c3
Greg Hogan <gregjhogan@gmail.com>
2021-01-27 05:15:59 -0800

    vision-only radar toggle (#19849)
    old-commit-hash: 767ada98c750a4d2bf2bbf36dc6dbc869123090f

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9962c664d70562915035ebc5776524c691e599aaf837b2ac3cd324bde065164b
-size 17124
+oid sha256:9b45b6089a1cf149911fed6af2cdb33ab0ed9cca3ef7e70e61df47418e0f922f
+size 17156
