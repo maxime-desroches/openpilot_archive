97093482bf9c7be10eca3e375651db63876278a0
Dean Lee <deanlee3@gmail.com>
2021-02-08 10:00:56 +0800

    removed margin paramater from car_space_to_full_frame (#20017)
    old-commit-hash: 40919a6f484042fc486c5da7d30b2fcc1d778e60

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac40a46e7b0037f142fd70e4719fc42832b40e74ff35e31a30aeb878ad80ba53
-size 22772
+oid sha256:2786a36bd6afc747f74bc74f9494019d381bf9f6e6f9846c1105c065c54520f0
+size 22789
