f499ca09fb80bb88b7b3d3fa82016e33cc43cb7d
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-02-10 16:39:39 -0800

    remove gpsd (#20027)
    old-commit-hash: fe7f3f0ec6b66e42b26451e180e3cde241e749bb

--- selfdrive/manager.py
+++ selfdrive/manager.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35665ceb2b2912603be0952eb49b82ba3e4f693574c2a397e7fb3f7f3c378364
-size 17180
+oid sha256:72b8f81d8aad99b9743a524c44aaed8470dbbf2d23c5fbd50439f725eee15fdf
+size 17123
