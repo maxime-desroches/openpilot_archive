6324ab3ada7e1bfadf924e558eb1d81ee96cf38d
Dean Lee <deanlee3@gmail.com>
2021-02-19 18:27:54 +0800

    replace sleep with util::sleep_for (#20108)
    old-commit-hash: ad88e62a8d4670cf24f00793c423c816629f9ec5

--- selfdrive/camerad/cameras/camera_qcom.cc
+++ selfdrive/camerad/cameras/camera_qcom.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c4fe7c39b1dcb7906b0a1e6cd2aecd24aec942107908995aea16220885ed752
-size 64941
+oid sha256:8f6b2103e257360270d0699d8fd19bccb51e08753a130cf75141f35dc27cc951
+size 64974
