21a2d06668fd6648f6c2dd91ac81ee1cbcb3f858
Dean Lee <deanlee3@gmail.com>
2021-02-26 21:42:27 +0800

    camerad: remove param pix_ptr from set_exposure_target (#20157)
    old-commit-hash: 2f718d28e4d04e174530c983800da9c6e052ca7a

--- selfdrive/camerad/cameras/camera_qcom.cc
+++ selfdrive/camerad/cameras/camera_qcom.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f16df6cd95fea7810cc617688b3bb80590f2bc9adeb1376b9d47803d0416202
-size 65330
+oid sha256:d493d9cf6d3246dc54b1983769df9d9f71b5993b9bd8cd815cbfed7a3632c509
+size 65294
