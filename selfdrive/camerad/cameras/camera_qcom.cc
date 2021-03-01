3e6f5e77f2be6807388faabe17377ee98c50a6a2
Dean Lee <deanlee3@gmail.com>
2021-03-01 19:17:30 +0800

    (msm_sensor_power_setting*) type conversions are not required (#20191)
    old-commit-hash: e6c47916a2bd67032956137449389cd11c1a5e43

--- selfdrive/camerad/cameras/camera_qcom.cc
+++ selfdrive/camerad/cameras/camera_qcom.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d493d9cf6d3246dc54b1983769df9d9f71b5993b9bd8cd815cbfed7a3632c509
-size 65294
+oid sha256:181801155e5efcc8b7fed104b6d58f11ea0810e6b2742609c28b0dc26a4bb02a
+size 65138
