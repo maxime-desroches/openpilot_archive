fb124496b1ad729b53093db3ff3376e93426ca0d
Dean Lee <deanlee3@gmail.com>
2021-03-03 20:10:17 +0800

    move function calib_frame_to_full_frame to ui.cc (#20230)
    * move calib_frame_to_full_frame to ui.cc
    
    * cleaup includes
    old-commit-hash: a1b2bdb844432b7cf59de412c57e9ee7f27fa650

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07d91fe9322b84da697ca491aea0d7bd270b9d4ac30b191ed82e5c6a79e46b9e
-size 22944
+oid sha256:75154cf0db35cb0d097537d2e2f00bc19dca943d5f7a7a6bef0432fa4f8ec581
+size 22235
