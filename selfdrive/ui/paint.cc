283845220dde6257619e7a3137125ad7f5cc783b
Dean Lee <deanlee3@gmail.com>
2021-03-11 12:52:01 +0800

    paint.cc: fix indentation & code style (#20302)
    old-commit-hash: 4e9e751ba5c070bd55ab7f421b8d8b00033d429a

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dec6e70a2dd7a51b3914a76e09c0fb2b394be66553bb2cad0e4aa475a7377cf1
-size 22265
+oid sha256:a163e7e1d92f387b9e2504445754fc9bc78e5ec280091db8d6774445176c04a2
+size 22280
