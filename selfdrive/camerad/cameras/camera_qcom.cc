0804c8296df935b47a3a56ea71ddf8010bed5906
ZwX1616 <zwx1616@gmail.com>
2021-03-12 17:40:50 -0800

    set_exposure_target test (#20318)
    * build
    
    * remove junk
    
    * clean up
    
    * clean up rebase
    
    * new patterns
    
    * add gts
    
    * add to jenkis
    
    * this more useful
    
    * typo
    
    * test only
    
    * Update Jenkinsfile
    
    * test flag
    
    * remove from jenkins
    
    * these should all just be common:wq
    
    * oops
    
    * unigt
    
    * add to unit tests?
    
    * build all is fine
    
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: 65bb979c34f3941044eb6a78f94037b2d4321418

--- selfdrive/camerad/cameras/camera_qcom.cc
+++ selfdrive/camerad/cameras/camera_qcom.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f407b0546ce54e7032514b7ec29c39808981bdf48301b2aad3215d82167f1d1
-size 45016
+oid sha256:7302e467fec1eb231b6310a6c35074297adbb0b4af7f60e8ce4fc900eb4037d9
+size 45058
