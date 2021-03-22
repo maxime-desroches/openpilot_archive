e2bb5c4e2f3fa5db83cb25323c3e417f3e5c897f
Dean Lee <deanlee3@gmail.com>
2021-03-22 11:27:16 +0800

    cleanup ui_draw_vision_event (#20386)
    old-commit-hash: 00dcc94ae650629dd82a0e63ba71d7dbc6d8f23b

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a163e7e1d92f387b9e2504445754fc9bc78e5ec280091db8d6774445176c04a2
-size 22280
+oid sha256:9470f73eff96b1e26bf872e19c9c5cc1c931f148b51a8433eac73e4c4a4d481e
+size 22112
