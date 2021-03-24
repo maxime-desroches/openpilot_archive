357c4c9e3e1a7260cc13c6c02d5a87f52ae3b2ff
HaraldSchafer <harald.the.engineer@gmail.com>
2021-03-24 01:10:31 -0700

    New KL model + laneless toggle (#20454)
    * New model: 0bf38240-6b55-42b0-bcfb-da08ff5cd0b9/650
    
    * laneless baby
    
    * hardcode model difference for now
    
    * kale toggle
    
    * kale emoji
    
    * sensible description
    
    * make clear it is not just for ui
    
    * emojis dont work yet
    
    * 9671a8fe-539f-4a04-a163-54571df21139/650
    
    * new model
    
    * only read param on rising edge of ignition
    
    * update toggle text
    
    * fix that
    
    * update refs
    
    Co-authored-by: mitchell <mitchellgoffpc@gmail.com>
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    old-commit-hash: 7ee79c99236f749d02a6779b006ee1ab187e0612

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9470f73eff96b1e26bf872e19c9c5cc1c931f148b51a8433eac73e4c4a4d481e
-size 22112
+oid sha256:0006fd61ca7137db52bcb58707dc9caf5703874c0c8ffaf1aac8c817c78e1b37
+size 22344
