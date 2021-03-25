aa293123741432c0a79a0e96c2bda81d031bdaa7
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-03-24 21:18:12 -0700

    Qt touchups (#20465)
    * reduce minimum size
    
    * clean that up
    
    * consistent
    
    * feels pretty good
    
    * cleanup
    
    * less laggy toggle
    
    * openpilot
    
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: 7f56991b41c40a765125635919a91bca55815bd6

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0006fd61ca7137db52bcb58707dc9caf5703874c0c8ffaf1aac8c817c78e1b37
-size 22344
+oid sha256:990e0db562a4dd77a96d1c0eca5fb2ff6305e91840ead471b3dec83cf6db4db9
+size 22291
