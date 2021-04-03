474dfdad76fdc94c8e7c7b9b74acd6051063e70f
Dean Lee <deanlee3@gmail.com>
2021-04-03 12:13:37 +0800

    camerad_qcom: init sensors with array (#20275)
    Co-authored-by: ZwX1616 <zwx1616@gmail.com>
    old-commit-hash: 26fa6e1644855bed5463e6c98bb11bf29cec3495

--- selfdrive/camerad/cameras/camera_qcom.cc
+++ selfdrive/camerad/cameras/camera_qcom.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cdb1d19865f0dea278d20fa9f4a31bf0fac97fad6f529f3667a991653e393263
-size 44391
+oid sha256:96b96153e028ee09df69e454680c5d90f0273e92c573fb28aa9d5701bcb08de4
+size 43835
