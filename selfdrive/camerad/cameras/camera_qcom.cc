b9e1f6058ff2798093fc4abaafbd79a8228cdc02
Dean Lee <deanlee3@gmail.com>
2021-04-03 12:10:48 +0800

    init frame_length in camera_init (#20261)
    old-commit-hash: 540b853c9f7e7a6a8db808bd2b1f000d6168cc4d

--- selfdrive/camerad/cameras/camera_qcom.cc
+++ selfdrive/camerad/cameras/camera_qcom.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9a23d17a1b04411f904351577853e671821ee6fc89c93c0c5ec1b0c00d3f14e
-size 44590
+oid sha256:cdb1d19865f0dea278d20fa9f4a31bf0fac97fad6f529f3667a991653e393263
+size 44391
