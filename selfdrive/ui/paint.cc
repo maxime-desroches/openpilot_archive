7da8bc1eef764848967b5b48608ac5eb9f7d5e00
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-04-05 13:46:26 -0700

    hide set speed for stock long (#20585)
    old-commit-hash: e4d7ee3716bd68d4128ea13796fec5d527293012

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3012e46d9841c57efc20ede0276b2f5bfe6a66e06ec04073a160630c40c5320
-size 22521
+oid sha256:0382313598174df62c903af211c82919dfd1f633637eb042d3cbfddd69089b9f
+size 22566
