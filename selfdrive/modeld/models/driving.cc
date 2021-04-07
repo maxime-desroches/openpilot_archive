c4b70b89e2ed6e4ccc5d57c57bbca4ba25c8fa48
HaraldSchafer <harald.the.engineer@gmail.com>
2021-04-06 23:49:29 -0700

    no heading cost (#20594)
    * no heading cost
    
    * live mpc weight config
    
    * need to add stds
    
    * make work on empty data
    
    * no divide by 0
    
    * update refs
    
    * update model replay
    
    * update proc replat
    
    * new model replay ref
    old-commit-hash: d72d433ec7a3c9f0bb2f90d196b3826d48d9b1a0

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a5b4f48f6878e35cfb70e6f8c88c2ae0dcc78a4e29f49a3b353aece60bbfcce
-size 11150
+oid sha256:502787efb000cbfa8a9461de896caca2cd9c3a805c1b32f04a66f60128f06017
+size 11031
