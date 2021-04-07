0fb5197fbd9ec128047af2a02f82bf45636c57fa
Willem Melching <willem.melching@gmail.com>
2021-04-07 19:12:35 +0200

    ecam toggle (#20597)
    * use ecam in ui
    
    * needs reboot
    
    * use in modeld
    
    * typo
    
    * effective FL is longer in center
    
    * normalize zoom by focal dist
    
    * read param before init
    
    * make 2x as wide
    
    * review comments
    
    * more explicit
    
    * fix camera offset
    
    Co-authored-by: ZwX1616 <zwx1616@gmail.com>
    old-commit-hash: 1fc7bcca43e9ef75d95ae8689e3b1cd8c4f8041e

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0382313598174df62c903af211c82919dfd1f633637eb042d3cbfddd69089b9f
-size 22566
+oid sha256:24e5de12eabe8b9d93166b26ba193b4b3ce5faafd5afa9090ccf3fff174581a0
+size 22748
