dd5a0788adf33a4a7559f7bd59821d73733202b2
iejMac <61431446+iejMac@users.noreply.github.com>
2021-04-22 22:13:32 -0700

    cereal: SubMaster refactor, update is now void (#20730)
    * cereal: SubMaster refactor, update is now void
    
    * bump cereal
    
    * mistake
    
    * update void
    
    * checks
    
    * semicolon
    
    * Update selfdrive/camerad/cameras/camera_frame_stream.cc
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    
    * check sensorEvent
    
    * update cereal
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    old-commit-hash: 4866a39244c59f540895ad64103e13f7c41a3cc8

--- selfdrive/camerad/cameras/camera_qcom.cc
+++ selfdrive/camerad/cameras/camera_qcom.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4752be2b2af62313959dd19890ffc01a36e1368e4ba40206f97b64a9b514baba
-size 43835
+oid sha256:dd8a7957c6ad490256d6dbc78b25ebb5335b3de34998b1f7795b10de686b67cb
+size 43860
