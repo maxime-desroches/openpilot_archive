4730b906632105d7dff59bc5656a194626f4de59
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-04-29 11:18:59 -0700

    UI: refactor GLWindow (#20764)
    old-commit-hash: 140e6248e26cfa370ccdb13fda8edc4fae0321ca

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d6efb0d97a3174f2a8143d72e51af80503e28b25cf4f14807b8ad4e081317ec7
-size 22874
+oid sha256:ce92a15970c75ad73cb53ad9eba35b6ab5bf63c1d5c4cd5b8b6b13cbb1b2ed22
+size 23086
