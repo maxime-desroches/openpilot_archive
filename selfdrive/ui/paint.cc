d5dd278534c6c631962fc212f0f867e98cd49c29
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2021-04-29 15:21:40 -0700

    small UI cleanup
    old-commit-hash: ebceadf08905a40f7e019c6b680111ec2ee787ef

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33c0a2e20af1f8bda0b05ac8189c85b5089512a6e186672368a3043a9629951e
-size 22922
+oid sha256:fabf66c54a22c72ab9335e8fb23dbceab07bab972b05d94e202675691ea7b960
+size 22276
