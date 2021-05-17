1727c0a831ab5669e2e93aa41a311c49070d82fd
Willem Melching <willem.melching@gmail.com>
2021-05-17 14:57:07 +0200

    Mapbox widget (#20751)
    * squash mapbox stuff
    
    * only hacks for pc
    
    * no hack there
    
    * only update when needed
    
    * get destination from param
    
    * no need for user agent
    
    * add athena method
    
    * change nav path color
    
    * layout cleanups
    
    * margin top
    
    * add build scripts
    
    * rename header file
    
    * set pitch
    
    * fix icon blinking
    
    * keep both options
    
    * draw on top to fix last blinking
    
    * only recomput with gps
    
    * fix include
    
    * put map in onroadwidget
    
    * update mapbox plugin to allow specifying directions url
    
    * cycle through views
    
    * dynamic resize
    
    * only when present
    
    * add map_helpers
    
    * whitespace
    
    * small fixes
    
    * let scons decide
    
    * update setup files
    
    * implicit dependency
    
    * fix alerts
    
    * Update selfdrive/ui/SConscript
    
    * move clearLayout to util.h
    
    * only build when map.cc present
    
    * move maps to own folder
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    old-commit-hash: 86aefbe7667f0693402c1883d286ce331e9d6215

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:19e89ff14c265f4a2abcaa351fffd504ebd9fbd8747548507e5c2f9083da3db6
-size 20190
+oid sha256:cc9555a02cc2819b6924bcafb49ff48b0519a97b47b4ed674a610f5f20cd52c7
+size 20317
