cc7d34ce9bcc0350dfbe1b5611a23399d44b19fd
Mitchell Goff <mitchellgoffpc@gmail.com>
2021-05-19 22:30:23 -0700

    New model outputs (#20933)
    * New model: 51a237cd-9c43-4281-9072-802325e6744d/950
    
    * Added new meta outputs and proper interp for plan_t_arr
    
    * Added meta-based trigger for FCW
    
    * No nans in planner, separate alert for model FCW
    
    * Cleaned up plan_t interp logic
    
    * Added empty cameraError alert to fix unit tests
    
    * Use the normal FCW event
    
    * Updated filter logic
    
    * Updated model replay ref
    
    * Moved disengage predictions into their own cereal struct
    
    * Updated model replay ref for new cereal structs
    
    * Apparently this breaks stuff?
    
    * Bumped cereal commit and model replay ref
    old-commit-hash: 29cc3500a5161b4d2245b8d041f190533be4ee8e

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb5b63d6f66588369323f8fd54b0fc00989acbee61824bbfff309aeef81fb6b5
-size 11070
+oid sha256:431b89b2c9d01305e2109e4b50b1a625f2ad1e65a83c3b7395b18bd3c35afda4
+size 14191
