5e35b2592a1147859eeb5c8314f71d4b0297f78d
HaraldSchafer <harald.the.engineer@gmail.com>
2021-07-07 19:44:17 -0700

    New desire model (#21458)
    * New model: b822ee32-917c-4639-99f8-25be3e6aabad/900
    
    * Enable keeps
    
    * use new packet
    
    * Send keep pulses once per second
    
    * Updated model_replay_ref_commit
    
    * New model: 0f160bf9-1bce-43e6-9785-564b2453eeac
    
    * Bump model_replay_ref_commit
    
    Co-authored-by: mitchell <mitchellgoffpc@gmail.com>
    old-commit-hash: 4230d5d2124577b86e69b136d846ca9cc3ecd746

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77d91d3a2bc9df1687a3a9eca9d6fbf64e3fcf2cdb73bc9db52d096968d415cb
-size 13370
+oid sha256:2a04cb5168efd918c32043235354691d22d62aa280baa7629173199cf01cc31f
+size 14222
