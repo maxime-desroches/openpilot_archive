fa70ae58810a5fce3a639ff238859da30b3742cf
HaraldSchafer <harald.the.engineer@gmail.com>
2021-08-05 20:35:03 -0700

    Draw model leads (#21864)
    * draw model not radar
    
    * compiles
    
    * wrong way
    old-commit-hash: 276b00ccb805b8071db4352b406f39233c2c3867

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26fef9639d0211cfd29cdeb0da66320d5507b6d401deb7a5b8f5453ab5a09e0e
-size 15153
+oid sha256:9fe9423d47b63d9785d65276e1feb868fd21ef4aa4aa2c3e10668bbf322ee28f
+size 15154
