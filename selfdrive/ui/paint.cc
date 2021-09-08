1478987b665565da8e475099927f97169c7f93b6
Dean Lee <deanlee3@gmail.com>
2021-09-09 04:13:27 +0800

    Qt/onroad: use CameraViewWidget (#21821)
    * use CameraViewWidget
    
    * no timer, updateFrame after frameSwapped
    
    * use QOpenGLShaderProgram
    
    * merge master
    
    * remove that
    
    * new function setStreamType
    
    * continue
    
    * remove showEvent
    
    * cleanup
    
    * cleanup
    
    * little more
    
    * fix black screen on startup
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    old-commit-hash: a034926264cd1025c69d6ceb3fe444965f960b75

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5354f252a24011e09e124b969d7bcb808e3ad8ed3a07bd6f3bc3b43eea5aa1d5
-size 15146
+oid sha256:dd9f1f9a2d91612449c32aee7322aa40a9093f31d5a6bc3401e71ee4d028cb5d
+size 11206
