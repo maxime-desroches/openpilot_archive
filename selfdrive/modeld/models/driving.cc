08b362ce6335755c235b13edab8f517676ea0136
George Hotz <72895+geohot@users.noreply.github.com>
2021-09-16 14:13:03 -0700

    Use thneed directly on the loaded YUV data (#22236)
    * completely untested
    
    * it builds now
    
    * bug fixes, save 1ms
    
    * using a kernel to copy works
    
    * more sane API to loadyuv
    
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: 83ff9ca3314b3d551949c822a98290713a0917bf

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57560e4c41cc4e1c93cc91ca9d3c9e95874eb4b20305d06582c332fa71898f13
-size 14269
+oid sha256:8fe400ee07bdf364a77113d8bc64e971f96b14c525a427f1b58fcd4a25f97014
+size 14367
