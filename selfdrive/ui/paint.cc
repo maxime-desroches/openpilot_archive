001a9b765a1e1a9689df4126af75d0c3fe4fa70a
Jason Wen <47793918+sunnyhaibin@users.noreply.github.com>
2021-09-30 17:45:24 -0400

    UI: whitespace fix (#22383)
    old-commit-hash: e797439c5f8d924ec5fd8b32b9a71e1c65c07ea1

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dd9f1f9a2d91612449c32aee7322aa40a9093f31d5a6bc3401e71ee4d028cb5d
-size 11206
+oid sha256:02c4009c6b5157f8a885f3dc5868321b9707ce8a2e191b8d15b481d774174199
+size 11207
