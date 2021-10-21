24b4838ea737bcfc564267b8f600ad2c2c8494de
Greg Hogan <gregjhogan@gmail.com>
2021-10-21 14:59:07 -0700

    ModelDataRaw struct part 3 (#22639)
    * lane lines and road edges
    
    * roll back some changes to find what broke things
    
    * rollback more changes to find issue
    
    * fix order of lane line probs
    
    * try outputs on stack again
    
    * initialize output array
    old-commit-hash: 755a0a63a2ff7d62ee3eef723a761548e4b437a5

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe1cb837ac926508082381c3a440220d2af0c1e3de9d085c9d3bb47951006fa4
-size 16385
+oid sha256:fad938782dea165e8a1b49b13c3ef05b58776f3bfce41970bef938a6075f2426
+size 15452
