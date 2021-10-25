6ee53797ac3a1fb32c13ba2c5f5c2ec04b03e0e2
Greg Hogan <gregjhogan@gmail.com>
2021-10-25 16:46:09 -0700

    ModelDataRaw struct part 4 (#22673)
    * convert model data for leads to struct
    
    * make things more consistent
    
    * change names with path to plan
    
    * consistent use of plural
    old-commit-hash: 7bb13acc45c645c222bc22831008ad45f30e6a7d

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fad938782dea165e8a1b49b13c3ef05b58776f3bfce41970bef938a6075f2426
-size 15452
+oid sha256:40334d925c7244a859e82b6240eead9aebea45fa9c011eefca875a8a96003ffb
+size 14865
