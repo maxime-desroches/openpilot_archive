ff031a5383d4ff9b1cd199911edc7015f77ca7aa
sshane <shane@smiskol.com>
2021-11-17 07:29:54 -0600

    ui: draw all radard leads (#22901)
    * draw leads detected from radar since it has a low speed override
    
    * comment
    
    * fill lead_verticies
    
    * fix
    
    * Revert changes
    
    Revert "fix"
    
    This reverts commit 7a478c435a0913e09512801309cf52654f3e17ac.
    
    Revert "fill lead_verticies"
    
    This reverts commit 5cdf265bb98480413a4a83c093b3e00489ba2f94.
    
    Revert "comment"
    
    This reverts commit 24a8a3fc2beef263909810d828d4e4fb5ea576e8.
    
    Revert "draw leads detected from radar since it has a low speed override"
    
    This reverts commit 4679fa5d16c7e961def9aaa52e92fb7d994a830d.
    
    * Revert "Draw model leads (#21864)"
    
    This commit reverts f1003b8c85f73d9fbecd0b83bc9a6de6f46ea0c0
    old-commit-hash: fa55dbe98394b5ed39385f26d6314a45fa899c15

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:28cfa23de6034781f1c2d545255e9b229a2b95ec208373434c55135fc3117968
-size 11169
+oid sha256:6179df3493e8dc4ceef581d0b6a76d60f250fbbe759cf6c48bd5fb36c07f08f7
+size 11166
