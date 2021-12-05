93f11ea62806bbd754339b7f72bfb3bc91afa843
Dean Lee <deanlee3@gmail.com>
2021-12-05 15:02:10 +0800

    UI: move onroad hud to Qt (#23040)
    * onroad hud
    
    * improve boudingrect, same as nanvg
    
    * QRect
    
    * Update selfdrive/ui/paint.cc
    
    * stacked with nvgWindow
    
    * cleanup ui.h
    
    * rename to road_view_layout
    
    * no lambda
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    old-commit-hash: ad09507e1ba2d758fe435d06a3dc28def33c14e8

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6179df3493e8dc4ceef581d0b6a76d60f250fbbe759cf6c48bd5fb36c07f08f7
-size 11166
+oid sha256:20342bef44694b47b70535635a8abf5fb1dc9ae53d7fc2322dd832ffaa68f23b
+size 5779
