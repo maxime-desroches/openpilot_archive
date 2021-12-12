2c5948980351e741aebe4010ada39994e2814081
Dean Lee <deanlee3@gmail.com>
2021-12-13 06:42:23 +0800

    util: move all functions into util namespace (#23203)
    old-commit-hash: 9decd3d8a20167de684322a458e28c8d1f746f83

--- selfdrive/camerad/cameras/camera_qcom.cc
+++ selfdrive/camerad/cameras/camera_qcom.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2f1cd4971e733c1447a9d6db3a631987389719641ca9d5b22f7d957eef3c1f7b
-size 44527
+oid sha256:9e67efb5d2f97d3f32f6ab99b46ab883256e8fd4cb2d6a0486b5a62bebd58e0f
+size 44533
