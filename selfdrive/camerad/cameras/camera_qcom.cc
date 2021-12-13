a90ea2b84cbbda082bd65b294273e18f8414fc3a
Dean Lee <deanlee3@gmail.com>
2021-12-13 22:53:44 +0800

    LapConv: remove hardcoded FULL_STRIDE_X (#23200)
    * fix wroing stride_x
    
    * remove hardcoded rgb_stride
    old-commit-hash: b519acd37bc4b160fc367bd690a326459f70109f

--- selfdrive/camerad/cameras/camera_qcom.cc
+++ selfdrive/camerad/cameras/camera_qcom.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9ccac4a5248cd70c54bb0b652135d0de75dcca9abfbb92ff5fa2d9c787fb063
-size 44408
+oid sha256:90c4ce5afa473f955f4902c43165a9a3f9120b0ac187d23a53c1a3ca76e827d4
+size 44436
