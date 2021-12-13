19a45432c34554296f86f8a3084ce16ef90d9bd9
Dean Lee <deanlee3@gmail.com>
2021-12-13 22:07:31 +0800

    common_process_driver_camera: same parameters as process_thread_cb (#23202)
    old-commit-hash: 5c6229f3df7ab499fa0a6121a327d9d538a2c847

--- selfdrive/camerad/cameras/camera_qcom.cc
+++ selfdrive/camerad/cameras/camera_qcom.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9e67efb5d2f97d3f32f6ab99b46ab883256e8fd4cb2d6a0486b5a62bebd58e0f
-size 44533
+oid sha256:a9ccac4a5248cd70c54bb0b652135d0de75dcca9abfbb92ff5fa2d9c787fb063
+size 44408
