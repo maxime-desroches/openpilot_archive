0f0db18be9650debe126136ee6a8d2dfe2d584c5
Dean Lee <deanlee3@gmail.com>
2021-12-28 05:31:42 +0800

    posenet_publish: use const references (#23310)
    old-commit-hash: 1fc1382480f2d346762bf91353f9f582b4d7d07e

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4cacdb855613ee1975a46a94d9df1789a9718a2275ea718a23a66adb5801ce4
-size 14531
+oid sha256:0b6c158f51a5fec255423b7097e69a8cb71c5feee2f698d28823f2da9f713ac8
+size 14559
