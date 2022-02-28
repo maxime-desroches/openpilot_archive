2841f34ad3a3da0e87393856d094185158bdcea7
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-02-28 10:32:39 -0800

    modeld: remove support for small model (#23803)
    * modeld: remove support for small model
    
    * use extra
    old-commit-hash: 8a19d9892efacc7078f1dbbddf45afedd8afe2ec

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b013d690d704cebfaa22d99bec7425990b1f3f2b7dd3ecb61e0672cf4852484
-size 15119
+oid sha256:70536e9597c1f3b9f28e883c5c8cfc5b0d1591fe82957a4361f6edb21f9422aa
+size 14822
