3f5073db340d47ab0fb745330c75945424ceb6de
Lukas Petersson <lukas.petersson.1999@gmail.com>
2022-04-05 21:05:45 -0700

    Latency logging 2 (#24058)
    * msg_order and gantt
    
    * frameId in long/lat planner
    
    * track frame id
    
    * controls frame id
    
    * graph tracked events
    
    * graph json
    
    * cloudlog timestamp
    
    * c++ cloudlog
    
    * add frame id
    
    * bug fixes
    
    * bug fixes
    
    * frame id visionicp
    
    * bug fixes and debug level
    
    * timestamp log placement
    
    * print timestamps in table
    
    * translate events
    
    * more logging
    
    * bug fixes
    
    * daemon boardd
    
    * print logs with boardd
    
    * more timestamp logs
    
    * cleanup
    
    * remove publish logs
    
    * bug fix
    
    * timestamp received
    
    * timestamp received
    
    * bug fixes
    
    * use json lib
    
    * ignore driver camera
    
    * prep for new timestamp pipeline
    
    * bug fix
    
    * read new pipeline unfinnished
    
    * read new pipeline
    
    * bug fix
    
    * add frame to controlsstate
    
    * remove controlsstate
    
    * print
    
    * cleanup
    
    * more cleanup + bug fix
    
    * clock build issue
    
    * remove unused imports
    
    * format durations
    
    * increase speed
    
    * pr comments fixes
    
    * conflicts
    
    * set MANAGER_DAEMON for boardd
    
    * clean script code
    
    * bug fix + argparse
    
    * remove rcv time
    
    * bug fixes
    
    * print without tabulate
    
    * fix pre-commits
    
    * plot gnatt
    
    * color bug fix
    
    * read without timestampextra
    
    * bump panda
    
    * mono time instead of frame id
    
    * finnish script
    
    * clean unused
    
    * clean unused logging
    
    * monotonic + json fixes
    
    * del test
    
    * remove whilelines
    
    * bump laika
    
    * cleanup
    
    * remove deps
    
    * logs nicer strings
    
    * remove plotting from scirpt
    
    * reset pipfile
    
    * reset pipfile
    
    * nicer strings
    
    * bug fix
    
    * bug fix
    
    * pr comments cleaning
    
    * remove plotting
    
    * bug fix
    
    * new demo route
    
    * bump opendbc and panda
    
    * cereal master
    
    * cereal master
    
    * script less komplex
    
    * assertions
    
    * matplotlib
    
    * readme
    
    * Update README.md
    
    * graph html
    
    * design fixes
    
    * more code design
    
    * Update common/logging_extra.py
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    
    * whitespace
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    
    * Update tools/latency_logger/latency_logger.py
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    
    * pr comments
    
    * bug fix
    
    * readme + env once
    
    * clean swaglog
    
    * bug fix
    
    * Update tools/latencylogger/README.md
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    
    * revert
    
    * revert
    
    * clean swaglog with error
    
    * remove typo file
    
    * revert graph
    
    * cereal
    
    * submodules
    
    * whitespaces
    
    * update refs
    
    Co-authored-by: Bruce Wayne <batman@workstation-openpilot2.internal>
    Co-authored-by: Comma Device <device@comma.ai>
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    old-commit-hash: 65fca83abed98f32993286dc5a66e3e583f06172

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cb64beaf38a04948f4e351a5581966af1107f2d1396930453eb84f043aa5ab49
-size 14899
+oid sha256:6fd0221fbb876177f2e4e626e2b576692ac70bb8a50bee61b286187c03e1a04e
+size 15021
