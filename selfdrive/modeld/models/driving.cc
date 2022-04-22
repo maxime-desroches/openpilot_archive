d75d03caa4302009d4ff8be3ce3094bb4e2ef8c6
Willem Melching <willem.melching@gmail.com>
2022-04-22 22:59:49 +0200

    Do not run model when skipping frames (#23949)
    * Do not run model when skipping frames
    
    * also prepare wide
    
    * add logging
    
    * remove newline
    
    * small skip test
    
    * more random
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    old-commit-hash: 374190873349fc9cb59771dbd60ca1a759ff3b8c

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6fd0221fbb876177f2e4e626e2b576692ac70bb8a50bee61b286187c03e1a04e
-size 15021
+oid sha256:cd60302f9921b3312f3901e4324bc9fc5f0d38b79718527d5e8cdd1c3ec5f0e1
+size 15088
