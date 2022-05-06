471fd3156fd43e805a2b04d566be1fffdb374c1a
Gijs Koning <gijs-koning@live.nl>
2022-05-06 04:41:14 -0700

    Laikad: the basics for ublox msg processing (#24359)
    * Add laikad that receives ublox messages and publishes corrected measurements and position fix
    
    * types
    
    * cleanup
    
    * laikad version 1 with uncorrected measurements
    
    * push
    
    * Fix glonass frequency and delete redundant test
    
    * Update after cereal and cleanup
    
    * Add test, fix laikad and remove process replay for now
    
    * update laika
    
    * add hatanaka to packages. Used to decompress orbit data
    
    * Fix pip
    old-commit-hash: b64fe6e339c8b8b3f5016a9c58b553c91abc5bdf

---
+++ selfdrive/locationd/laikad.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5f088ba2526fcbf2182ff12b5c9900da0e65b447301de88f6b33d17d49416426
+size 3052
---
+++ selfdrive/locationd/test/test_laikad.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:75de945e0e52820729524051519021a5b0511494414e2452e026b1e1923d42fa
+size 661
