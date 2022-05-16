b13ef5bae77b5d154a82ec030119d22e8110a74b
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2022-05-15 17:29:09 -0700

    move models to selfdrive/modeld/ (#24539)
    * move models to selfdrive/modeld/
    
    * two more refs
    old-commit-hash: 319f3d1475f2f212b63f32dbace476f63b3caf14

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd60302f9921b3312f3901e4324bc9fc5f0d38b79718527d5e8cdd1c3ec5f0e1
-size 15088
+oid sha256:bc6017e49aac290851f411b5e1d7cd3835339f981edb8582d6e99890076fbc5b
+size 15070
