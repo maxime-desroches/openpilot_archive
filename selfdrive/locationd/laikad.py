35a4833f2bf040f24f2b83278eae15fc705fb042
Gijs Koning <gijs-koning@live.nl>
2022-05-20 03:21:17 -0700

    Add gnss kalman filter to laikad (#24578)
    * Add gnss kalman filter to laikad
    
    * Fix analysis
    
    * Fix analysis
    
    * Update laika
    
    * Refactor a bit
    
    * cleanup, improve unit test
    
    * Also commit test
    
    * fix
    
    * Always return messages. Use Measurement struct that contains std and valid keys
    
    * Push cereal
    
    * Change prints to cloudlog
    
    * Always send valid messages
    
    * Only send gnssMeasurement msg when measurementReport type
    old-commit-hash: ef97329c0e825d477577bf27cbaf772753f077ca

--- selfdrive/locationd/laikad.py
+++ selfdrive/locationd/laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:23b644dfbaa7affc8fdf7730e5f7a46d4994bd79523d130d6978cf4afc21095b
-size 2924
+oid sha256:218a3afb12b56779bd363db348b97abea6481482004bc8c2df7c0ffb0e74ee08
+size 5952
--- selfdrive/locationd/test/test_laikad.py
+++ selfdrive/locationd/test/test_laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b86d24ef53f34464f047837b15909f0a0f3d272d3bab7070413b07a3a848d2af
-size 651
+oid sha256:2a300b6f00350975974128fa30eef60e84ec6d765c5ff7b8a2f529d08b8b4a8a
+size 1814
