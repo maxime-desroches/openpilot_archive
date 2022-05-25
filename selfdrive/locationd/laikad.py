8ba2d6ffc814d673e3b2e7b5d2379fc35ee99df0
Gijs Koning <gijs-koning@live.nl>
2022-05-25 17:15:22 +0200

    Remove scipy usage from laikad (#24652)
    Remove scipy
    old-commit-hash: 65056b77c22334b4be727151732bf2a76f9b780d

--- selfdrive/locationd/laikad.py
+++ selfdrive/locationd/laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:64398fb29c42e6b28c3d5b3dfa8c40b03ef4dc4ba358d06ceb440b6b9ed6a013
-size 6434
+oid sha256:146edf77df9612df3c68fd53b82738dbe75912ca7a3dc26b44a07f503414e6c2
+size 6441
