81afd43e77c6c212beaef2e3a9d3635a4a6b00b4
Gijs Koning <gijs-koning@live.nl>
2022-06-03 12:12:31 +0200

    Fix test_laikad in master (#24734)
    * Use Livelocationkalman measurement struct
    
    * Revert laika repo and fix time gap
    old-commit-hash: ad04585c35afdc885ca3c24c066e5801c25d2bc7

--- selfdrive/locationd/laikad.py
+++ selfdrive/locationd/laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:146edf77df9612df3c68fd53b82738dbe75912ca7a3dc26b44a07f503414e6c2
-size 6441
+oid sha256:6e3c1ff20681090700ec43ff482edcc232149cfed1585af3fee3c98370986fd9
+size 6456
