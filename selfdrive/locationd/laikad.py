0b17bd93d64d6b0745478863dbf384b6e9780de7
Gijs Koning <gijs-koning@live.nl>
2022-06-08 12:32:22 +0200

    Laikad: kill process correctly. Fixes CI (#24780)
    Kill process correctly
    old-commit-hash: 45c43f9c8719efc300a0cb373f1dc57e0abe3ece

--- selfdrive/locationd/laikad.py
+++ selfdrive/locationd/laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b53050cc874715de2893dce994e7f545fe90712f28682f98d68dbae4707cb181
-size 7867
+oid sha256:e8a14e1e0432ebe708484c41a85275c325c44bcf286ca8ea781d4424e45e8f53
+size 7948
