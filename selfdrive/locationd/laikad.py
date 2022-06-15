f704ec34bae1660098bd8005947cb1cd683a8366
Gijs Koning <gijs-koning@live.nl>
2022-06-15 14:00:27 +0200

    Laikad: Remove bearingDeg from message (#24864)
    * Remove bearingDeg from message.
    
    * Push cereal
    
    * Commit cereal
    old-commit-hash: 9e3d0e3c9cea4dcf2da10801aad041b1dd65cccb

--- selfdrive/locationd/laikad.py
+++ selfdrive/locationd/laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f5c8da3b528ffc20e8934db522b7c950b536d87fe9e8f2464b9b85a6bfbe612
-size 10552
+oid sha256:a737528dd1ae80e5e1c025cea38504997bf6e5612057d5e85803377aba6a5509
+size 9907
