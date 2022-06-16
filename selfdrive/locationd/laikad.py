199462c1cf17083959574fe8d69d20ee20c29612
Gijs Koning <gijs-koning@live.nl>
2022-06-16 17:45:08 +0200

    laikad: fixes to run on device (#24879)
    * Always run laikad on device!
    
    * Update laika
    
    * Update laika
    
    * Fix gps week and time of week in msg
    
    * Reset kalman filter if pos_fix or last_known_position
    
    * put behind file
    
    * move pr parsing into common file
    
    Co-authored-by: Willem Melching <willem.melching@gmail.com>
    old-commit-hash: dc98511b7a7092e969237655b6e08e4fa857106d

--- selfdrive/locationd/laikad.py
+++ selfdrive/locationd/laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b49ab7e8945c772d734c02f216235ad4c05103469406b9bf1c26ea92065c4b0
-size 13214
+oid sha256:88f47d001a2058e2b7f6b5bcc6b0f12fb2433a418fc7d9ed7051eb1a7757a44d
+size 10974
---
+++ selfdrive/locationd/laikad_helpers.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:56151415baa6032b2e1b0e707ff6e2b8b9cde19997fc841f7a07843de43921fc
+size 2643
