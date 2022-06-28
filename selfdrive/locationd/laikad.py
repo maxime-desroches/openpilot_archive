cd83a6df1c2887cf6451a463b0044ecf84a785d6
Willem Melching <willem.melching@gmail.com>
2022-06-28 13:43:15 +0200

    add laikad to process replay (#24889)
    * merge
    
    * Fix closing process executor after fetching orbits
    
    * cleanup
    
    * Add ref commit and revert test_processes hack
    
    * Fix
    
    * Fix ref
    
    * Fix test
    
    * Temp
    
    * Temp
    
    * Trying
    
    * Trying
    
    * Cleanup and change test
    
    * add ref commit
    
    * remove print
    
    * fix test getting stuck
    
    * cleanup fetch_orbits
    
    Co-authored-by: Gijs Koning <gijs-koning@live.nl>
    old-commit-hash: 3823f55476b56fd3db0afe686b1d2f3d508817bc

--- selfdrive/locationd/laikad.py
+++ selfdrive/locationd/laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fdc4c7eea3bcee7c9fcc48e57965ae0ba0617f65a4a846e5df507e10f8125047
-size 11465
+oid sha256:d142b8ae9e8f699d814174b0375bf3e5202ce3e9e3f31402b468237647113feb
+size 11781
--- selfdrive/locationd/test/test_laikad.py
+++ selfdrive/locationd/test/test_laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30df001d26b6bb4bc458753f55b3939b28650a6c859fc600f02dbde1cdd64057
-size 7830
+oid sha256:5b24b7c1e6b6b214e8c785e6a7c4f544e0567bd01d4ef85adb9218239fddb25e
+size 8631
