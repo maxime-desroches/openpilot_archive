fd516bf8ac8b8a00712782469180531541f8fb23
Gijs Koning <gijs-koning@live.nl>
2022-07-01 17:44:10 +0200

    Laikad: Allow fetching orbits every minute (#25016)
    * Allow fetching orbits every minute
    
    * Small cleanup
    old-commit-hash: 8d6799d95a0a0ab9b2b0bc4ed6998abf6cabf266

--- selfdrive/locationd/laikad.py
+++ selfdrive/locationd/laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:56eb13efced5f78842bac36aa8a8cd5d6bb9c08f9107bc0a6a1879f6d3015550
-size 12681
+oid sha256:ce7db018a9b353638115f6585932b6890236737fa4e36997780ce146c5c0190a
+size 12642
