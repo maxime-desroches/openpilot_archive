a29890cea2545b27697cd6dfd5808abd3719d046
Gijs Koning <gijs-koning@live.nl>
2022-07-07 13:06:51 +0200

    Laikad: Use clocks for faster fetching orbits (#25060)
    * Use clocks msg to for first fetch of orbits. Which is sent earlier than ublox msgs
    
    * refactor last_fetch_orbits
    
    * Add comment.
    Add test
    
    * increase timeout
    
    * Add clocks to process replay
    old-commit-hash: eaf7eb42784732136e52cd031035e9c5ab520e6e

--- selfdrive/locationd/test/test_laikad.py
+++ selfdrive/locationd/test/test_laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9582083f29a0ffc8daddf5aaa8e94b41b3ae1e5a2fc03ca79647d9e4e1b262b1
-size 9031
+oid sha256:710d803368101f064ac78b673db1e4177f772694c37459f8826fff63cc782cc4
+size 9857
