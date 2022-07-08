592c6393dbbd12fd2e69bb0188a7201a266a97c3
Gijs Koning <gijs-koning@live.nl>
2022-07-08 19:03:42 +0200

    Laikad: Fix getting covariances for pos and velocity (#25084)
    * Fix getting covariances for pos and velocity
    
    * ref commit
    old-commit-hash: cf862b6576a8b3f66e26d38080a1c8eec00f0793

--- selfdrive/locationd/laikad.py
+++ selfdrive/locationd/laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a512578bcce3838217ee27eb81c7c2302ca30822b31873d7334ca1f77b135ed
-size 13509
+oid sha256:e17950c1ddd44c788b41101eff15d50e1f1d7208f61405bbbb14e9276c84cc0a
+size 13487
