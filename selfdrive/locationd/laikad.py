19ce909ad867df0ffce17a23226e60eabe1d6a20
Willem Melching <willem.melching@gmail.com>
2022-07-08 18:20:16 +0200

    laikad: add residual threshold for pos_fix (#25082)
    * laikad: add residual threshold for pos_fix
    
    * update ref
    
    * update test
    old-commit-hash: 7a4c33795a12eefff4c3e5c311f40eae2cf3506b

--- selfdrive/locationd/laikad.py
+++ selfdrive/locationd/laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a3ce5e0585af7991f1ca3e475f40115d37a893f5da946553d2c80c8b33dddda4
-size 13405
+oid sha256:3a512578bcce3838217ee27eb81c7c2302ca30822b31873d7334ca1f77b135ed
+size 13509
--- selfdrive/locationd/test/test_laikad.py
+++ selfdrive/locationd/test/test_laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:710d803368101f064ac78b673db1e4177f772694c37459f8826fff63cc782cc4
-size 9857
+oid sha256:aa045b847bcdfc6a7469439db48de449092b62798a2969ce770bf7b989d9ade9
+size 9855
