5a2c66a83497f6b41a7a22305d4c988b30b3fa7d
Gijs Koning <gijs-koning@live.nl>
2022-07-14 11:49:27 +0200

    Bump laika and catch Downloadfiles exception.
    old-commit-hash: c7771d1783ef7d7e1efb29e81aae44ad26747c5c

--- selfdrive/locationd/test/test_laikad.py
+++ selfdrive/locationd/test/test_laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa045b847bcdfc6a7469439db48de449092b62798a2969ce770bf7b989d9ade9
-size 9855
+oid sha256:122305f9ad8e8037b92fa8745c30df6782e119570dae362d3fb1ad65acde573f
+size 10311
