c99c2c9080304ffea2f435688ac8b4a56051a430
ZwX1616 <zwx1616@gmail.com>
2022-07-29 13:52:24 -0700

    Remove RHD toggle: determine by driver side instead (#24802)
    * driverview
    
    * auto choose
    
    * useless
    
    * remove
    
    * modeld not use toggle
    
    * remove from params
    
    * should have commissue anyway
    
    * delete icon file
    
    * deal with init more elegantly and prohibit switching when engaged
    
    * nonblock save when not unstable
    
    * filter very low speeds
    
    * a few more secs dont harm
    
    * draw icon
    
    * match
    
    * radius
    
    * use const
    
    * ref commit
    
    * new transs
    
    * put_bool_nonblocking
    
    * 5k route tune
    
    * ref comm
    
    Co-authored-by: Comma Device <device@comma.ai>
    old-commit-hash: 22408781f7d7df5ff0395f80c1b15d6407acd0ff

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87a9aa9aabb346db60af85456d2fc4fd7db0519739f5f589201c31a228501ade
-size 15090
+oid sha256:879a98edd5fcdf982362751625b322906178a9e90c73c6e92ec91d7fa9db61db
+size 15121
