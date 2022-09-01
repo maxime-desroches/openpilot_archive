ddfdf486b370b4683276ab9196d0fdfcca887df0
HaraldSchafer <harald.the.engineer@gmail.com>
2022-09-01 15:40:25 -0700

    Support e2e long in longitudinal planner (#25636)
    * refactor
    
    * Add planer modes to support offline, acc, and blended
    
    * add acceleration
    
    * Fix index
    
    * Update model ref
    
    * Read in model outputs
    
    * Add model msg
    
    * Add e2e logic
    
    * Add source
    old-commit-hash: e1b7a37a1f6a341bdc729c186ca5b4d3fb8af9ee

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0e01826fcce689b793f1b470e8a9e0751ef9994bd22b46d9a27be641042c778
-size 15161
+oid sha256:fe84e59db7f85ceaf0343cfe9e25284ed1433dc5b30b6292ac0d39ac55ac064b
+size 15426
