cab8336f7816e0269889eb7c969181e8bd99c386
Dean Lee <deanlee3@gmail.com>
2022-10-07 04:57:11 +0800

    cabana: Docking and undocking charts (#25983)
    * floating dock charts
    
    * more button
    
    * setMinimumSize
    
    * move reset zoom button to title bar
    
    * show chart count
    
    * cleanup
    
    * reduce flicker
    
    * dont update linemarker if pos not changed
    
    * cleanup
    
    * remove blank line
    
    * always show dock/undock button
    old-commit-hash: 9ec262bbfd09c7cd12773cabab0bb6d933b4d13e

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:02dd82d3858a884b371b06309a2ec3f11093d8ddb5dc205e2d962ef901042bc3
-size 6905
+oid sha256:b0f9f5b845f342e0a08809cca8ec3aeb988fff73cb737aecdceabac71e5dd415
+size 9182
