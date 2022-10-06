f2e9411cfa0567d9b1f91013c208689169a35ed1
Dean Lee <deanlee3@gmail.com>
2022-10-06 12:17:22 +0800

    canbana: complete basic functions (#25965)
    * add chart header
    
    * get all signal val from logs
    
    * loop in selected range
    
    * clear list before append
    
    * automatically zoom on yaxis
    
    * cleanup
    
    * sync charts
    
    * fix event_begin_sec
    
    * set the color of rubber
    
    * add TODO
    
    * sync slider with charts
    
    * keep video aspect ratio
    
    * sync plot buttons
    
    * reduce flickers
    
    * cleanup
    
    * refactor detail view
    
    * clear counters
    
    * more
    
    use qcamera
    old-commit-hash: a6ba073231761e06ac6f070a01b434243d9d0693

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91c22cca5b255b3ba1e2c08bd8ef5d77993b08c192dad452a30de87d0f8a85c5
-size 3265
+oid sha256:493bb66b59c9846832662e977b15bf5fac26c90c2eb4bbf0633d623b6d0850fe
+size 6709
