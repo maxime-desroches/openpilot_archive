3134448f3870fc949ffb11e99ba2b709d8030898
Dean Lee <deanlee3@gmail.com>
2022-10-07 04:35:56 +0800

    cabana: Fix the incorrect Y axis (#25984)
    old-commit-hash: 2d9e7972598918a72de736e12edbc2967d77e841

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e96832f3abe895aa591fc16e03c821748bbdca0467fb2564c3c99a0fdc531e2
-size 6715
+oid sha256:02dd82d3858a884b371b06309a2ec3f11093d8ddb5dc205e2d962ef901042bc3
+size 6905
