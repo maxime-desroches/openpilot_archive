7ba85cd181b8ce35b8f3f3d7a46e9940c1cf3b81
Dean Lee <deanlee3@gmail.com>
2022-10-13 08:04:53 +0800

    cabana: keep scrollarea frame (#26056)
    add frame back
    old-commit-hash: b65fad9e8f232167bcc0fc9139bd632c573abb60

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0b4f7b11cd1ebaa974ae24710f613aba9a0064418fce49cb7475d5dd08217f5
-size 10366
+oid sha256:57b6ce2d1a8a122910e6cab3141f791c60d30e3014db20fcc042b9b782368bd5
+size 10317
