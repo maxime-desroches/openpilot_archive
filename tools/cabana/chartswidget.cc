43d95f2d7ac21b2e46cb195892e9a26e4a855998
Dean Lee <deanlee3@gmail.com>
2022-10-18 05:39:18 +0800

    Cabana: align the charts properly (#26116)
    old-commit-hash: 60586e0d5835dbb3928321700c9d4d99cdeec216

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35eed31ba53a3dcda2548eac76185d362564cfe884cf9bb9e66c12d2fa11fe99
-size 11540
+oid sha256:d4a712721407af29a2c46cd5442f1d94c17e9322b795191de4f2216a06a4218e
+size 12150
