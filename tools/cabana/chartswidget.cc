4a31453a7bc52fce47fe21f95b57272e3166f8fd
Dean Lee <deanlee3@gmail.com>
2022-10-18 12:00:36 +0800

    cabana: fix chart margins (#26125)
    old-commit-hash: 544526edeb343d4b6f9c7c6f2521659c34d37012

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d4a712721407af29a2c46cd5442f1d94c17e9322b795191de4f2216a06a4218e
-size 12150
+oid sha256:9c678e058c9a3090887cbb9df66dda3762e1b5cc19bc0dfd418866e3aaf21d28
+size 12201
