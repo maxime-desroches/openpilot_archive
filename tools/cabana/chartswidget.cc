3697efac1128a532a73a5c49187b893c64df9c0f
Dean Lee <deanlee3@gmail.com>
2022-11-04 11:09:14 +0800

    Cabana: update viewport after mouse released (#26355)
    update viewport after mouse released
    old-commit-hash: 241d88c018d2acae4dbc71b5a033644959e2b24a

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ca4fa92c86a5c6ae3a9de800aa67b80113178d012f9f93b08f080f021da9f08
-size 17042
+oid sha256:3ca95c2e4288367e78161904a80fefe0af0b5b2670880b1c3f4796253c44cc27
+size 17068
