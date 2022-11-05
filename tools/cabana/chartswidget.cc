f2614e57b61e64c3e1e8f4c6a8e05c10648a65ea
Dean Lee <deanlee3@gmail.com>
2022-11-06 07:52:16 +0800

    Cabana: cleanup code (#26369)
    * remove ChartView::enterEvent
    
    * cleanup ChartsWidget::removeAll
    
    * group graphics items
    
    * remove rubber->setPalette
    
    * helper function DBCManager::parseId
    
    * remove variable name from dbcManager
    
    * fix readme/Usage
    
    * use QSlider::setRange
    
    * cleanup include
    
    * use emplace_back
    
    * remove varialbe routeName from CanMessages
    
    * remove tmp variable
    
    * remove blank line
    
    * cleanup layout
    
    * clean settings layout
    
    * connect to streamStarted
    
    * cleanup signal/slot
    old-commit-hash: ac76cc93256578f24db255cefd10c013dd906c28

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ca95c2e4288367e78161904a80fefe0af0b5b2670880b1c3f4796253c44cc27
-size 17068
+oid sha256:7a1bf1336351a0bbc9f28ecbe5c41f55185f7755b9478adbef7bbbd3c1f3c142
+size 16323
