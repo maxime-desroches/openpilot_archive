c224fdab85458f87e5c647176ed0df7dcfaa45e0
Dean Lee <deanlee3@gmail.com>
2022-11-07 03:06:42 +0800

    Cabana: use QToolBar to manage the controls in ChartsView (#26388)
    use QToolBar
    old-commit-hash: 45891c79079ae4404b8828d9bcfe5cd79bce0c90

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d855136400b60b72d4cb27c0144a0dcff177551239001c904d20a9b36a295189
-size 15224
+oid sha256:ce76cbcd7cc688c2fa84e735a149fe372f27536ee41dd90edb93cf06ae241338
+size 14758
