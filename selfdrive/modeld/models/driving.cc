bb44648560cd8894a50b08b2e59c772cde962b58
Harald Schäfer <harald.the.engineer@gmail.com>
2022-11-07 17:49:36 -0800

    E2e long model: calibrate model speed to wheel speed (#26395)
    * calibrate!
    
    * Fix test
    
    * Fix proc replay
    
    * check len
    
    * get v_ego from model 8501d20-bb59-4193-aa82-82b2737dedd6/449 609d90f3-65e6-4617-a60c-d6d99eead408/700
    
    * bump cereal
    
    * initialize v_model_error
    
    * typo
    
    * better names
    
    * cleanup
    
    * bump cereal
    
    * update model replay ref commit
    
    * bump to cereal master
    
    Co-authored-by: Yassine Yousfi <yyousfi1@binghamton.edu>
    old-commit-hash: f63f0de80a78ade6ad12fe98ea27094035d97457

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:59fd5ef8896d87f41daa56df4243bc5e5cc59efa807c16776a59bcf172603055
-size 16234
+oid sha256:3f5c90dcc5f57891665dd3e314683812d167bde48c9d4cff238fd7253e8ee898
+size 16814
