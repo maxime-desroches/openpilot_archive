15b80e62fca279adfcb2d1e7d433ca4ab37f08f6
Dean Lee <deanlee3@gmail.com>
2022-11-10 06:19:11 +0800

    Cabana: support deleting message (#26418)
    delete message
    old-commit-hash: 59bf2fc0085420b13b7854d1a106c1706d7df265

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:51484c97d6ff33df149ea6ed930c1aebd232a32036dc63c2e9653fe94f619c2b
-size 14805
+oid sha256:acbd2bf6a55ca5e1842f5a53572adbc4aad5091d21b602e508101b213968d542
+size 15022
