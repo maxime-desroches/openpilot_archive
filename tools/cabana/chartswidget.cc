9bb817b3d9a21309f17d4499e97e110e73a42752
Dean Lee <deanlee3@gmail.com>
2022-11-11 02:37:38 +0800

    Cabana:  Added support for undo & redo  (#26440)
    * undo/redo
    
    * display command list to rolling the state backwards or forward
    
    * update detailview after rolling states
    
    * add * to title bar to indicate dbc has changed
    
    * fix signal pointer address changed after removed
    
    * cleanup
    
    * fix id error
    
    * clear undo stack after dbc file changed
    
    * cleanup
    
    * use map
    
    * cleanup
    
    * typo
    old-commit-hash: 7c922eafe9d9c8e07de80f8bafcd9b2dc2932c13

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:acbd2bf6a55ca5e1842f5a53572adbc4aad5091d21b602e508101b213968d542
-size 15022
+oid sha256:21f0dea8e805495e56f31e3144e9a2af0913def416765aee09dec78fa5be1405
+size 15014
