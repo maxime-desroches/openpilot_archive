125d1892d1071b081f16dff8438fa60956773ece
Dean Lee <deanlee3@gmail.com>
2022-11-15 02:04:14 +0800

    Cabana: Fix title overlapping on chart with long names (#26494)
    fix title overlapping
    old-commit-hash: 29f24c7491d74ff2d2f7ca12e9a085dbe91c93fb

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2df7ab95182d7b1eee909d2fee469eb291ca81f2f24b2845ef001d3207c5fdcc
-size 15124
+oid sha256:c0ca6df160bc7f5e2219fbfe13e5981bbf4da00b5a881e0346b4ed48a8c17983
+size 15019
