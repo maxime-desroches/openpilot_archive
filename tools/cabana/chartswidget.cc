44fc93aedc90498919d317cd42820fbefa84255d
Dean Lee <deanlee3@gmail.com>
2022-11-22 02:48:08 +0800

    Cabana: fix auto zoom y-axis for multiple line series (#26558)
    auto zoom y-axis for multiple series
    old-commit-hash: 7b46928fc9cda9e5200d72c15253f60981eedc74

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad93065cdf2cb848beb7416749d3fbcc67dc40c430cf30ff6c24b314cc68538e
-size 16369
+oid sha256:c3498fb9bfa414c51618f47e87ecdace26e8b142a038e7dbc2217f8efe2ec640
+size 16512
