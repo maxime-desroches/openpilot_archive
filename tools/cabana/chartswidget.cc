55d98475932f56f587614fb2ffa9f2a397004644
Dean Lee <deanlee3@gmail.com>
2022-11-23 06:23:49 +0800

    Cabana: auto theme, detect from system. (#26563)
    * auto theme
    
    * cleanup
    
    * get font color from ForegroundRole
    
    * fix label color
    
    * add padding for header
    
    * smaller warning icon
    
    * fix bg of binary view
    
    * hightlight after init
    old-commit-hash: 4662f1e0be77d6636ef7e3f663fb839448d86af6

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a8e2fc8606856cadf1ee224a6cfcc2552dba7f30a4e8cf497a61b1b5d34b240
-size 16635
+oid sha256:6e6e366019ef7d4382739522728dfb5f337d47e871987b7a870ff26f8c546ff1
+size 16750
