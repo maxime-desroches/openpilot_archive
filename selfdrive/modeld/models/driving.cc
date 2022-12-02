5ae71955358ecf4676342857a98e8471d41fa54c
Mitchell Goff <mitchellgoffpc@gmail.com>
2022-12-02 13:14:30 -0800

    navmodeld (#26665)
    * Added navmodeld
    
    * New nav model: 7c306685-5476-4bd4-ab65-105b01b6bca8/300, feats only
    
    * little cleanup
    
    * Remove NAV flag
    
    * Moved to_kj_array_ptr to commonmodel.h
    
    * Switch from decimation to last_frame_id check
    
    * add to release files
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    old-commit-hash: bb8a38a0508c764c8340e7b16968b06a8367ab7a

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f5c90dcc5f57891665dd3e314683812d167bde48c9d4cff238fd7253e8ee898
-size 16814
+oid sha256:ef3a798cd4d8b6d4cae0d76c0bb48757d448592262dd20a85a0664b3a92bd399
+size 16645
