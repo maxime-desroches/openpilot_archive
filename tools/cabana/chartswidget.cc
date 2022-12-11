a3339ebfd093b77850f169cc10d51b432753c238
Dean Lee <deanlee3@gmail.com>
2022-12-12 07:00:58 +0800

    Cabana: use opengl to draw the line series. (#26761)
    old-commit-hash: 4e3598c9f60e16bc48cbbb3152d74a5d89bebbea

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3de150322d2b80ad9220e48455438830a454c06a0e6d60189046724ed8d2963
-size 23486
+oid sha256:3c31b24c3e86dbe9c4f5638a8f5dee8433bbcfd45e9bcdc4e8b2ebf2d4b3f48a
+size 23150
