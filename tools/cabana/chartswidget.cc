cb3b94f9459fb2f163b6bee4c178fd69d84e5798
Dean Lee <deanlee3@gmail.com>
2022-12-14 12:25:29 +0800

    Cabana: add auto completion for Msg selector (#26788)
    old-commit-hash: ffb709e6a68c8dd254f96bbf05f6799370352e7f

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1334b1842bd1b13fbfff9506cd49ee86616c4b80c59b936ead2e53c6fb9878b4
-size 23288
+oid sha256:8d27ed05f026e4abee276704650fee555a1251fb1f0624d684b7ba3142a977de
+size 23618
