afea1bc9bb2b76618331292b48ebcbd1bad8d6d0
Dean Lee <deanlee3@gmail.com>
2023-01-16 04:52:11 +0800

    cabana: support drag and drop to merge charts (#26968)
    old-commit-hash: 58bd02408979f19999acd8c755ed30785b9ef6d7

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b88b3c48e8772bddaa0a65601164f012a07812b9661b2a266cbb8aae5e47cbb7
-size 24384
+oid sha256:4c2df90a4b928da4a9652b88f54e34c5d4112aab95699df6c34aa0bb18f7ea92
+size 25994
