ffc3f7e6ea586087689b6931cece2f2494795ee8
Willem Melching <willem.melching@gmail.com>
2023-01-16 07:13:16 +0100

    cabana: MacOS fixes (#26973)
    * make qtcharts a framework
    
    * cabana: dfisable openpilot prefix on macos
    
    * cabana: disable opengl on charts
    
    * fix linux build
    
    * no newline
    
    * add comments
    old-commit-hash: 5cc192be4d994d26e385f03a1945d810d465eb00

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c2df90a4b928da4a9652b88f54e34c5d4112aab95699df6c34aa0bb18f7ea92
-size 25994
+oid sha256:029043918370e513748f5cb37f54d0ea1b27565378adafb959422c3a6259b903
+size 26153
