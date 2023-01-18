362da7c28419a96e4ecef345c58d9900296a421e
Dean Lee <deanlee3@gmail.com>
2023-01-19 05:19:23 +0800

    cabana: use bootstrap icons (#26981)
    * use bootstrap icons
    
    * typo
    
    * build into asset_obj
    
    * add to files_common
    old-commit-hash: c21d9408a191ba8e3303f2459d1f7476fefd35ba

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c0a67c4c3332d606415a0d36c7468b4ba503d29faba6e18c888564f2a9098ca
-size 26106
+oid sha256:df473b3c83c91e6ddd01e1b30a73fff9fd0a8906132bd22e677743368a46a17c
+size 26308
