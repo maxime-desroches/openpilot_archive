0073e5672d8f88af38fb020f19d11076c14a61dd
Kurt Nistelberger <kurt.nistelberger@gmail.com>
2023-01-19 11:46:22 -0800

    Revert "Add low Gnss laikad test (#26987)"
    This reverts commit 453c31b007237ae5009c4a9e0bb3afa04aa12f84.
    
    old-commit-hash: 9131da9103718d6ae113ad7b3519037e2b4ffa70

--- selfdrive/locationd/test/test_laikad.py
+++ selfdrive/locationd/test/test_laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:851c09258210d04fac8cdfd3f91db54f0e142f921b96da2381169a43d8276bfc
-size 11102
+oid sha256:c22e3a8fd61c7bf830de16ccddb36ead486549a9c48f36eb1b9e98dd20bcbf10
+size 10085
