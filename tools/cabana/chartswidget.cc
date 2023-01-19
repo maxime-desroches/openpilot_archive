fbce14a2f59d5d332ef922877703eabd3a8a09cc
Dean Lee <deanlee3@gmail.com>
2023-01-20 07:26:00 +0800

    Cabana: support live streaming (#26946)
    * support live streaming
    
    * update live stream's time
    
    * cleanup stream classes
    
    * disable video control in live streaming mode
    
    * emit streamStarted() in LiveStream::streamThread
    
    * disable some features in live streaming mode
    
    * refactor charts to support live streaming mode
    
    * disable dynamic mode checkbox in live streaming mode
    
    * updateDispalyRange
    
    * thread safe events
    
    * TODO: add support for ZMQ
    
    * atomic time stamp
    
    * only keep settings.cached_segment_limit*60  seconds data in liveStream
    
    * make charts work better in live mode
    
    * cleanup ChartView
    
    * fix toolbar
    
    * cleanup
    
    cleanup
    
    * disable openpilotPrefix and useOpenGL on macos
    
    * add comment
    
    * exit gracefully
    
    * support ZMQ
    
    * use ::operator new/delete
    
    * cleanup streams
    
    * cleanup
    
    * align stream buffers
    
    * check looping back
    
    * check if series is empty
    
    * cleanup
    
    * add TODO: write stream to log file to replay it
    
    * upper_bound
    
    * remove class member event_range
    
    * change default settings value
    
    * cleanup updateDisplayrange
    
    * fix merge error
    old-commit-hash: f9490739ab37ae680aef272d51eb3cf2b36b4103

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:767bdc38a920790017c8219b6de55c0a2d1699c9fe3ca3b97c4d18fd3dfcc31e
-size 27854
+oid sha256:7d20685ed91045aa0a97bedc5a54fdece6331e1192d759933922c8a0dc743fe6
+size 28615
