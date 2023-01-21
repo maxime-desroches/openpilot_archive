a09aab6464c9917a1491bf2e4945507cd454c09f
Kurt Nistelberger <kurt.nistelberger@hotmail.com>
2023-01-20 23:19:01 -0700

    Laikad: add test (#27028)
    * add laikad tropo test
    
    Co-authored-by: Kurt Nistelberger <kurt.nistelberger@gmail.com>
    old-commit-hash: 027c27cd690120d8ff8823f0b53cc2c9d6af7df3

--- selfdrive/locationd/test/test_laikad.py
+++ selfdrive/locationd/test/test_laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c22e3a8fd61c7bf830de16ccddb36ead486549a9c48f36eb1b9e98dd20bcbf10
-size 10085
+oid sha256:77da943a07e8df3419fec68610c895bd1b9eb8a241af46b2ac86b0ea21cc0fb9
+size 11036
