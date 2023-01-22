5993cb9c7f9d9d05d9af41b2575f04c30cb88c38
Dean Lee <deanlee3@gmail.com>
2023-01-22 08:00:39 +0800

    cabana: add new plot btn to create chart (#27023)
    old-commit-hash: 35a943030ba67b89a13bdf95257e0b349ffb0ef7

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f96a31f89c73ae6aa373fc5e8ced5c46d7d3810f217dd8c60483617cd52e7b75
-size 27683
+oid sha256:eca52c254a6a919a5f0104af0d6a16ab94347d5112b9b6e896561cb24a0018b8
+size 28150
