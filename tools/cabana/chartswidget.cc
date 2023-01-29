9d3ca06dab7467cccbd5d13610db379455061208
Willem Melching <willem.melching@gmail.com>
2023-01-29 21:57:01 +0100

    cabana: show dots when zoomed far into a signal (#27145)
    * cabana: show dots when zoomed far into a signal
    
    * review comments
    old-commit-hash: f038193f4447afc8ae7b6034dec430d08f6da331

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f9dd016ba5d9f246070d63ab2cec91f78d26b2277481b42e8b45a027b35ef0e
-size 28310
+oid sha256:ea6ac14b535fe526de31a01fdba5828bfd673596e36d7ed59fc43839a0fe725f
+size 28786
