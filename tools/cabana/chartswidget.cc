4b3be5c280815b47496bb14de7568a3f667d90ba
Willem Melching <willem.melching@gmail.com>
2023-01-29 21:55:10 +0100

    cabana: fix occasional ellipsis on y axis ticks (#27142)
    old-commit-hash: bdb42f7f84516279f308f95496a5f5f901c4adc1

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:99c8071ee600da007b5cc29cc90c428294936519aec32f01c9b6a76aa72d1e3f
-size 28260
+oid sha256:6f9dd016ba5d9f246070d63ab2cec91f78d26b2277481b42e8b45a027b35ef0e
+size 28310
