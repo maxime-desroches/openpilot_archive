2a648cf209bdcefb91820101d3cb990e277c5471
Dean Lee <deanlee3@gmail.com>
2023-02-01 05:45:57 +0800

    cabana: render light or dark icon based on system theme (#27155)
    old-commit-hash: 0402e949270219bfbd964d38916ed52917090169

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9241ebc132e95e659cbc7f47cac6d53df282fb2f6dd56339c278704e8673e6ad
-size 29716
+oid sha256:691c4f0ce8a375455cb04ba267ac6f20d2e767a034aca31fee1d1e6918aea925
+size 29657
