b176012989aefc3ade280ff2bc1bc8129acf3e53
Dean Lee <deanlee3@gmail.com>
2023-02-03 05:16:32 +0800

    cabana: add capability for switching between line and scatter plots (#27169)
    * add capability for switching between line and scatter plots
    
    * Update tools/cabana/chartswidget.cc
    
    Co-authored-by: Willem Melching <willem.melching@gmail.com>
    
    * setUserOpengl in createSeries
    
    * update series title
    
    * set marker size by pixels_per_point
    
    * sync menu state
    
    * cleanup
    
    * set default series type in settings dlg
    
    * remove qdebug
    
    ---------
    
    Co-authored-by: Willem Melching <willem.melching@gmail.com>
    old-commit-hash: 53396076dfaa857da735d9943fc7e33cefdadf69

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a21b5265bfe31f5feb793c7df4df189c2c89e11234c5a72e880b75fd44b4b6a
-size 30369
+oid sha256:8924a6461f71236a693a6a5fee9c37dc2e0d76e2ceecd706355ac08c22acdef7
+size 32566
