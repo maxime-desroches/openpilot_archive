8b722aa26dc63125dfdcf52dd74a9e4f4849791b
Kurt Nistelberger <kurt.nistelberger@hotmail.com>
2023-02-02 21:16:28 -0700

    glonass support laikad (#27173)
    * add glonass kaitai parsing
    
    * add kaita generated files
    
    * remove glonass from build
    
    * add string non immediate type
    
    * fix kaitai bug
    
    * cleanUp
    
    * add patch file
    
    * fix scons order
    
    * make lookup const
    
    * remove comments
    
    * add to release files
    
    * init
    
    * add laika
    
    * remove printf
    
    * laikad add glonass ephem parsing
    
    * add signs
    
    * final ublox parsing
    
    * rev gps
    
    * bump subs
    
    * address comments
    
    * disable internet for laikad
    
    ---------
    
    Co-authored-by: Kurt Nistelberger <kurt.nistelberger@gmail.com>
    old-commit-hash: 04a7808590ac55a5e3dea6080b6a24aae78d9277

--- selfdrive/locationd/laikad.py
+++ selfdrive/locationd/laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f89aca9530290b7f156ab5bf424b198fed2f519c30ef453f996ffcc9e49316c
-size 18063
+oid sha256:c7ba9f89ca02066774aa2317c713f89e4844f687465add0db3987318e39347fc
+size 18492
