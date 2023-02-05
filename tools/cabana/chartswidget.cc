b10adc2bf1cf012fe9eada4a41d85de3f41463d4
Dean Lee <deanlee3@gmail.com>
2023-02-06 04:35:30 +0800

    cabana:  only relayout charts when needed  (#27210)
    old-commit-hash: 54f3ca43bb72f697a5a2fc90d3c26f7301a48cd0

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:069be446baca9b8ae666004245e3e82c3986fe2bdcc4289b328c89cbc2eb69c3
-size 32528
+oid sha256:a0a4ccb2c215ceac41e862ecbdbbde4acfdd9b45260488043a98f15988ec8eff
+size 32782
