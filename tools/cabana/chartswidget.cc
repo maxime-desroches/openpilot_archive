6fcee87013be725fad0797813c5185887b708f32
Dean Lee <deanlee3@gmail.com>
2023-02-07 10:57:37 +0800

    cabana: improve charts toolbar (#27233)
    * improve toolbar
    
    * add text
    old-commit-hash: 8f6521ded3c06b52f0d3c1389e4077be3f184329

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48e927b4df80b83d87665c3ebbbbaad4170734d30d0b74c5f095643cf2ef2b5d
-size 34222
+oid sha256:d7a0d8b145c73a50098a4a33c778e5250c6a224b1931f44e36904bfa44603b8d
+size 34490
