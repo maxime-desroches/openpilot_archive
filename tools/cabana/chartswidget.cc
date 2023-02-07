418cf035831a5931f8b49aa56bdc48e39c3839a6
Dean Lee <deanlee3@gmail.com>
2023-02-07 10:58:24 +0800

    cabana: paint points in drawForeground instead of switching opengl mode (#27239)
    old-commit-hash: ae99a6d15d363ac01f970f266dd7bc7a13ae7366

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7a0d8b145c73a50098a4a33c778e5250c6a224b1931f44e36904bfa44603b8d
-size 34490
+oid sha256:b4be38a340a4e4c7ac2fe183174e4d5cff4300e1ab96eeb69dc3d3c13d3cfcb8
+size 34471
