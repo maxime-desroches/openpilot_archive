3422f4743dafc6188f7f25c97120ee273b93a8aa
Dean Lee <deanlee3@gmail.com>
2023-02-07 13:14:07 +0800

    cabana: remove toolbutton padding (#27240)
    * remove toolbutton padding
    
    * fix incorrect checked state in setSeriesType
    old-commit-hash: 55ad7ee3cb50256bdc16c6777e2432bcf9f702eb

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4be38a340a4e4c7ac2fe183174e4d5cff4300e1ab96eeb69dc3d3c13d3cfcb8
-size 34471
+oid sha256:569d6702287bed8785a46a1ec8063cffd1efa7823fd0fa89f97747faa1f127ab
+size 34518
