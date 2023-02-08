1261a756d2d02108f45640eaa6e072bc3e1debb4
Dean Lee <deanlee3@gmail.com>
2023-02-08 13:46:59 +0800

    cabana: add space between signal and msg name (#27252)
    old-commit-hash: 583057aa413f533e761ce869abd69f2fedef39b9

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:569d6702287bed8785a46a1ec8063cffd1efa7823fd0fa89f97747faa1f127ab
-size 34518
+oid sha256:0716a7acd291fe24ef25370f673f085e5a33bf2dca34342d09f6a2a990a50043
+size 34507
