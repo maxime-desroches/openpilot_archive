5174accece901b5d9b512e40c8b317115fd19fa4
Dean Lee <deanlee3@gmail.com>
2023-02-11 02:13:28 +0800

    cabana: set pen width in createSeries (#27288)
    old-commit-hash: 409c1c9c6558850bbced8ba05bf125102e6543bb

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d0885c7801abfe14857ed27095cbb5ed6b70a4ed9b2ea31f3ac7424a43ecb250
-size 34852
+oid sha256:63198c8d6e358217517cab0a145d5e01ff56b93997b96a16b6b555f482bb6f96
+size 35016
