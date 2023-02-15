e277cdea9b6f30e9f54ee6fd423678158ea7b453
Willem Melching <willem.melching@gmail.com>
2023-02-15 21:39:03 +0100

    cabana: refactor message id from QString to struct (#27352)
    * cabana: refactor message id from QString to struct
    
    * fix tabbar
    
    * fix findsimilarbits
    
    * optimize hash function
    
    * cleanup sorting
    
    * use in updateLastMsgsTo
    old-commit-hash: 5078c918056c189a3ec84bc63231fbae75c57eda

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:832d391275190cdd5635cb66df6a1afd51617bc97c58cc11e2b5a2e00c8ad699
-size 34511
+oid sha256:ab275c3b3789655008dba1ea28be5727bf906721bb6a64b34f7f58c08eaf869c
+size 34520
