26b144e0daf884aad9fb0dac905860781092beaf
Dean Lee <deanlee3@gmail.com>
2023-02-16 02:59:57 +0800

    cabana: cleanup code for charts (#27350)
    old-commit-hash: e4d0ee8716c382e90fd725998216a2f7deafc592

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0c66ba26702fd1da94f9c7c7995f2af232ab0f82592031c57680e18dd55d1ccb
-size 35164
+oid sha256:832d391275190cdd5635cb66df6a1afd51617bc97c58cc11e2b5a2e00c8ad699
+size 34511
