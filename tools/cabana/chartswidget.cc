f65dacaeb02c0bf506f860095ef9727d13cdffb7
Dean Lee <deanlee3@gmail.com>
2023-02-18 11:31:54 +0800

    cabana: add support for load&save extra dbc info (#27203)
    * support extra info
    
    * support undo/redo
    
    * fix undo/redo
    
    * cleanup
    
    * fix regexp
    
    * refactor dbcmanager
    
    * replace text in headerview
    
    * fix binary::refresh
    
    * cleanup
    
    * use QRegularExpression
    
    * add desc validation
    
    * edit val description in table
    
    * cleanup
    old-commit-hash: ac0dbf74bc835faa46beab4bbb948c95597e4e67

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4dfddc045bf7770ea453c0adfa3099dabc2dbb84239c6911eaf702732ee5d3d4
-size 34619
+oid sha256:2caae63ec450e3c858964e22cea0d0eb42f04589cb4fff74e12e9bbccee7e0d1
+size 34587
