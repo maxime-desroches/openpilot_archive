3571da1fa60722d8d266aa1a1da9a0fb0ad90921
Dean Lee <deanlee3@gmail.com>
2023-02-28 07:51:23 +0800

    cabana: fix gaps in step line between chunks (#27447)
    old-commit-hash: ce3e9d5649d930ea0daae2d2a1587cc6e69c158b

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93c548178d9b045303a01841fbeb7a034a7e066a29c473295ee32b86ecd70d23
-size 35473
+oid sha256:ed53c19a92cefb8eb004044fad937b5bebb6eb1c0bf0814e6ca212716abc255a
+size 35654
