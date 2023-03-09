72d6087d13b5a14b24a09013b31a55704ac4c101
Willem Melching <willem.melching@gmail.com>
2023-03-09 22:14:06 +0100

    cabana: scrub by shift dragging on graph (#27545)
    * cabana: scrub by shift dragging on graph
    
    * not while live streaming
    old-commit-hash: b79f4594c42b4af0fc371b5d48445ce86c1cb3ff

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d2f4553dedca9234d9ba5357b7b013e84ca5ca9482a3e6d33d3417a312c126cd
-size 36109
+oid sha256:8fac458c9b5f4229013bcceb06058b2770557b2a9cbf3003688a8ee4a01075b5
+size 36920
