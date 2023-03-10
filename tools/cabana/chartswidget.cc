5a63e7ce689888ea678ce5c57af809d0fe5e3099
Willem Melching <willem.melching@gmail.com>
2023-03-10 20:19:37 +0100

    cabana: refactor Msg and Signal into dbc.cc (#27552)
    old-commit-hash: e4d591ecbfaaf12b267e3f93927a89e9b4f6a7b9

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8fac458c9b5f4229013bcceb06058b2770557b2a9cbf3003688a8ee4a01075b5
-size 36920
+oid sha256:0b1795563b7285fbf22af0298b5fbb6a01d7f1ae2c70b4315e5c3094818c7f36
+size 36976
