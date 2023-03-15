30b134d597c64210cbb40dda16e67014ca3e3d54
Harald Schäfer <harald.the.engineer@gmail.com>
2023-03-15 15:16:47 -0700

    Fix qcom laika caching and test (#27594)
    old-commit-hash: 629e5f6bf21460f1f50d0a56c131440e0ab4f0b5

--- selfdrive/locationd/laikad.py
+++ selfdrive/locationd/laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48c3823acc30f35c1eca47f0576fd1ef4ea8a9680657df373f47c639f4a7283e
-size 18534
+oid sha256:34ddc0ccb7a136a5e635b6b8072147f6d00f9eec16446f4d9bbfc88116c571dc
+size 18591
--- selfdrive/locationd/test/test_laikad.py
+++ selfdrive/locationd/test/test_laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a7b1a707b95ee9747af290db5dc1d9b6d537669bb3938993aa71fe4e20ef1e1
-size 12206
+oid sha256:2a85bf26c5adef36a01ef87621a9e3d2b2a26bf731aa64e451c50ec34be4348c
+size 13585
