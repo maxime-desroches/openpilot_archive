08eb8a46ef12d228d464192f38a20428f8e83deb
Harald Schäfer <harald.the.engineer@gmail.com>
2023-03-16 17:00:54 -0700

    LAIKAD: TTFF, and ephemeris statuses in logs (#27591)
    * ttff in logs
    
    * More meta info
    
    * cleanup
    
    * passess tests
    
    * unused import
    
    * fix linting
    
    * ttff of 0 is invalid
    
    * ref commit
    
    * bump cereal
    old-commit-hash: 95216de5910c62bf81a7bf6053b5917de98dc825

--- selfdrive/locationd/test/test_laikad.py
+++ selfdrive/locationd/test/test_laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0905a95ee5355ce6651b859beeeec58bd29c4dca838b8c2db392b19e3a0a885
-size 13556
+oid sha256:9afc716497320f080087600aa6dab3a09575232b1d4fda3a7c049343911e9a63
+size 12963
