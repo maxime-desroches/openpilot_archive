f047afe76287f1a97a023e069d64ae1e50fe2932
Harald Schäfer <harald.the.engineer@gmail.com>
2023-03-17 16:39:44 -0700

    Bump laika (#27618)
    * Laika on master
    
    * use constellation id
    old-commit-hash: 4718fa574813b966557e453f25ab65a56fbce8fa

--- selfdrive/locationd/laikad.py
+++ selfdrive/locationd/laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:70b814fbd6fa95f02336abcf9fbb156717a43f554d4adff9fdeb135eb7d8788c
-size 19464
+oid sha256:f8eb54159396f71483a198becbd10ebf21bdb48ba5f80a31dbca4458803a8c43
+size 19492
