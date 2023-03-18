d5abcc4402698b1c6b46ea690021947ba08bc054
Dean Lee <deanlee3@gmail.com>
2023-03-19 05:34:11 +0800

    cabana: display zoom range while dragging rubber-band selection (#27620)
    old-commit-hash: 529ba41825c3d47ee6f322ddecc8471d8e0cdd3a

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:164568bbfe8bb304ab13668f0226580cdb32ce952a898d385e21f94c75735e1c
-size 35138
+oid sha256:3d4bdfcf739509de993825d06daa47dd04412361c82e9234af82dee57159b4c6
+size 36060
