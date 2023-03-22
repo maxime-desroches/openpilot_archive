db851b05ee4f483c7ef2a68b01bd2afc40a91af2
Gijs Koning <gijs-koning@live.nl>
2023-03-22 01:07:19 +0100

    laikad: use non-conflating socket for gnss msgs (#24888)
    * Use subsock
    
    * wait for one
    
    * wrong name
    
    * try cpp proc replay
    
    * fix recv pattern
    
    ---------
    
    Co-authored-by: Bruce Wayne <harald.the.engineer@gmail.com>
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    old-commit-hash: fc5a575ba3aef8fd4b3b7bac93bbc557c3bee11a

--- selfdrive/locationd/laikad.py
+++ selfdrive/locationd/laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b23a40ee263080690b8f18fd66bc8a248130b1ca89bbf0e31831afd404c463d8
-size 19456
+oid sha256:b2dc1d84bc3112413471d7ffc37aae906852a39d66b47b31c8e2cd3872353bf7
+size 19493
