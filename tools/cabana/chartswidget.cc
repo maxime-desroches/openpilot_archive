1c402628b88b39bc885be8624f1550caeec11856
Dean Lee <deanlee3@gmail.com>
2023-03-25 12:54:41 +0800

    cabana: enables user to select the zoom area when the mouse press on the data point (#27679)
    zoom anywhere
    old-commit-hash: 33afefd346e4348aacb122a486066741731b7d05

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1fca47efd342e341efe20a60ddba5f2b1280b757e3fcadb3693b407102469b28
-size 36056
+oid sha256:53324753c46afce90e62e506e1914d20998917c6dfc61aab5d33e88c166398d4
+size 36432
