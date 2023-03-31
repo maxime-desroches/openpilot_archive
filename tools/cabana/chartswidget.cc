c9ed1ca0a4ebf94c4834a21e100e73a930227ad2
Willem Melching <willem.melching@gmail.com>
2023-03-31 20:32:27 +0200

    cabana: show min and max values in chart tooltip (#27744)
    old-commit-hash: ced90c163ed292f41a450810035ae32438fe81fc

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f70e3a410e0d603310f1f97de789d36ef95d38662559ce2ca6af50d6cc5a5d0
-size 38365
+oid sha256:72cc5f7da10b970ca66c0b4ad21100ed40dc7431a463c9e9033ed140c24e41b0
+size 38636
