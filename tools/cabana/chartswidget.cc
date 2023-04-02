171586c7125f667f55d160e045a1b1d52ac69fa1
Dean Lee <deanlee3@gmail.com>
2023-04-03 02:34:10 +0800

    cabana: support toggle between light / dark theme (#27787)
    add dark theme
    old-commit-hash: c56872265a126a0e8feda8bb21225e885ea6e941

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73b659a3b47aa4f3e72fa684d5b450bd496f8dfb30dcb06391d2e70650a94e53
-size 41297
+oid sha256:3fe196da34eeb20983f57830ad6d9cd492bf6e78bfa541bf416e88942bfdcdee
+size 41141
