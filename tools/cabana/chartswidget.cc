bba0e7a527079b0308b3c82768f2b9503fbd7030
Dean Lee <deanlee3@gmail.com>
2023-04-04 03:50:29 +0800

    cabana: dynamic update chart tooltip (#27790)
    dynamic update tooltip
    
    dont emit serieschanged if no charts
    old-commit-hash: 7c7347a9e6e108386bc5f85e214ca07a2e3ea0ee

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3fe196da34eeb20983f57830ad6d9cd492bf6e78bfa541bf416e88942bfdcdee
-size 41141
+oid sha256:9fd27af1aa2bbcfe74b3410170ee7b49a6fa4bcff5606786eb886f7f66ac4e27
+size 41299
