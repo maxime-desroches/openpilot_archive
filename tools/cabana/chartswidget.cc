4279ab959fb376a3dc034473f82b59499fb941a7
Dean Lee <deanlee3@gmail.com>
2023-04-05 01:46:36 +0800

    cabana: support undo/redo zoom (#27799)
    * support updo/redo zoom
    
    * cleanup eventsMerged
    
    * init axis_x when create chart
    
    * update tool bar after create chart
    
    * align charts after updatelayout
    old-commit-hash: c5f5a3b5f2063be36497c81526c3a4c42d84beb7

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d35a6b9d42e36b86f3432c2f51b1a7143b38421908b9f0f2efa1a93e9ba97b4
-size 41453
+oid sha256:cf620f2516c66704de0298883823b63b287cbb332bc9800447096a5b5a14ad9e
+size 41417
