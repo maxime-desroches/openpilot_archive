c3367ae04e79584f2c57a25d283f5d029b95e228
Dean Lee <deanlee3@gmail.com>
2023-04-08 05:55:00 +0800

    cabana: support multiple tabs in ChartsView (#27833)
    * support multiple tabs charts
    
    * new tab button right
    old-commit-hash: 424bd0c0b69043ced3b773555e0603b0e3ead396

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31dcb1c6d3d1d00d1fcabad1ff235699bc1369396779d28855038060e0ab1bdb
-size 45237
+oid sha256:82ccf6718dcdcd62c03faca3ee977223cffa9a5c5b84a38a0138e00edaad4845
+size 46882
