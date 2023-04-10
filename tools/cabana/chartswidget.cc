1365ef1798162dbf622cb358de917c13dbee2bf2
Dean Lee <deanlee3@gmail.com>
2023-04-11 06:53:50 +0800

    cabana: switch theme on the fly (#27855)
    * switch theme on the fly
    
    * better chart theme
    
    * Darcula like dark theme
    
    * add TODO
    
    * use theme aware toolbutton in ChartView
    
    * add margin to text title
    
    * lighter activity colors
    
    * fix historylog header
    
    * replace hardcoded color
    
    * improve chart tooltip color
    
    * use macro
    
    * space
    
    * remove function toolbutton
    
    * fix remove all btn
    
    * remove menu arrow
    
    * improve chart::removeIf
    
    * cleanup charts
    old-commit-hash: fc90814199d10b4419574f0e5a01102ec55c9cdb

--- tools/cabana/chartswidget.cc
+++ tools/cabana/chartswidget.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:791f83f9d6f4a0a571169df5c6411c699afb0105bea89d891d29062aa268cec9
-size 48694
+oid sha256:6dcc16a12908bac515d8607b8feabdf731741f2f5b1665c58e2228aa7a28a66f
+size 49401
