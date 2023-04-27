3520499823a79d132120d4b501c9a5465c29326d
Bruce Wayne <harald.the.engineer@gmail.com>
2023-04-26 19:20:05 -0700

    Revert "LaikaD: new proc_replay segment and no internet laikad (#28029)"
    This reverts commit 1341b59160ffa9db29d589f3fe4dd38691361165.
    
    old-commit-hash: 613bbcd62661eecda609cfa32be2e31a6bb490ca

--- selfdrive/locationd/laikad.py
+++ selfdrive/locationd/laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f89abe5574d945066fac0f382c47869dfef9273a32f343ce767003dce572067
-size 19634
+oid sha256:a60462c8ecc1749f5797fca61f1e89d03fac433e1a7834a48016d477e094cdf8
+size 19693
