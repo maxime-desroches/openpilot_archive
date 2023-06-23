fd33a355a9b5603306e60bcb90c490086e1c9bca
Mitchell Goff <mitchellgoffpc@gmail.com>
2023-06-22 19:39:51 -0700

    Publish navEnabled from modeld (#28647)
    * Publish navEnabled from modeld
    
    * Bump cereal
    old-commit-hash: 4c2c73b15282f405900b64b0fb67d461f3058b5c

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b3d82ae3951898e66d3dbc0e61b2942d1b652f09190a8caba1a2ea1c2c84173e
-size 17463
+oid sha256:7333e6900d1d7edb7bb96951c68c8ec9b7deb6b7a83e32362f8b7473955a9238
+size 17524
