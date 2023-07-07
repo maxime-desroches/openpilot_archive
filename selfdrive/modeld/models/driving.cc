8944d98fb23a0f06f6d5657d7636415df7a729e9
Mitchell Goff <mitchellgoffpc@gmail.com>
2023-07-06 22:26:11 -0700

    Refactor model runners (#28598)
    * Started work on model runner refactor
    
    * Fixed some compile errors
    
    * everything compiles
    
    * Fixed bug in SNPEModel
    
    * updateInput -> setInputBuffer
    
    * I understand nothing
    
    * whoops lol
    
    * use std::string instead of char*
    
    * Move common logic into RunModel
    
    * formatting fix
    old-commit-hash: c9f00678af17dc75b9e56a968c4357704e40c901

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1ef4e8804f4bdfec2b025ba6ba506a4f194a1905f9bc04737f9f2a1558e809d
-size 19191
+oid sha256:137b020dfbc8a1e82963074facb0e2288d3e2c64c628625e80ceb68763b50f09
+size 19466
