90f1a7d8c7d9d600f9aba7a3851de032bae769ea
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2023-07-18 18:47:35 -0700

    Revert "Update pycapnp and Cython (#29021)"
    This reverts commit 9f80430d3958cbcf4fb9b67a428f45fd034c5649.
    
    old-commit-hash: 9c307f2550ccade43409c9a5835203fdc355f26c

--- selfdrive/locationd/laikad.py
+++ selfdrive/locationd/laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:542396f5d82438c0109fbd716e17bf8088d39f2c99b37fd1fcce8e92c3d791fe
-size 20591
+oid sha256:ddfb01bcc7f2128894e69de7c7c34530e61032169528a207c8efb9df84a6f9ae
+size 20580
