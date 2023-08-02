0b5aa047e00534636f896cbb66d500f6cb671674
Mitchell Goff <mitchellgoffpc@gmail.com>
2023-08-01 19:23:18 -0700

    Clean up model_publish args to simplify cython bindings (#29203)
    * Clean up model_publish args to simplify cython bindings
    
    * pass by reference
    
    * Move FCW and model confidence queues into PublishState
    old-commit-hash: 663fc0d8fe7cb203259fceb5b8aef9fde270b9cb

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8807e360e4f09e9bab64355d8cb9eaa76e6bae1d45b80c8e7b913176bd44f9f4
-size 19535
+oid sha256:170fb03ff486291eb398c214303a75883826e4049e684b06477a508405e8ebb7
+size 19361
