550fe697394466b868e9ec1c4e677a25d36cd2ea
Justin Newberry <justin@comma.ai>
2023-08-09 13:26:54 -0700

    CI: Pylint to ruff (#29294)
    * pylint to riff
    
    * pylint to riff
    
    * pylint to riff
    
    * revert more
    
    * undo exclude removal
    
    * exclude tinygrad
    
    * set line length
    
    * pylint exclusions to ruff
    
    * same excludes as old linter
    
    * fix tools QA
    
    * remove unrequired check
    
    * revert linting third_party
    
    * ignore e402
    old-commit-hash: 7fdd9fc37c1c5febf1cd3dc86441ebf0dec7f370

--- selfdrive/locationd/test/test_laikad.py
+++ selfdrive/locationd/test/test_laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5cb5c118b4572462c35e851fec612f2247fe371a855c7d10fe604b2312e83a25
-size 12470
+oid sha256:e8cf57d4a167e02a3c4f61f805073abe805ac7e3548d6585fb789a08941d716b
+size 12453
