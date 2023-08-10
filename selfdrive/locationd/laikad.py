2338b7e4a89ec369938dc6ab122078be1cedb867
Justin Newberry <justin@comma.ai>
2023-08-10 15:28:51 -0700

    Ruff: flake8 pie (misc lints) (#29318)
    flake8 pie
    old-commit-hash: af3ed37c5cd8823f793aec60e1c74760cf795bd9

--- selfdrive/locationd/laikad.py
+++ selfdrive/locationd/laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85ff602c54ee3cd8ebde11e62d7fedf0db12fa1a80a6a7068a71d658b6664e9d
-size 20621
+oid sha256:637e69cf95a76cf0598724eb67e930f215c2a0a16d815cf7ff8d9c9111e1e939
+size 20608
