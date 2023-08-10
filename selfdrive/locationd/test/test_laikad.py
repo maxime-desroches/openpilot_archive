3a7870f3f7727825463d4babf9e2ecd2689873b2
Justin Newberry <justin@comma.ai>
2023-08-10 16:40:30 -0700

    Ruff: comprehensions conventions (#29317)
    ignore c408
    old-commit-hash: 4a9afd7554060ac5353e96f1d7feb06d3a3671e9

--- selfdrive/locationd/test/test_laikad.py
+++ selfdrive/locationd/test/test_laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8cf57d4a167e02a3c4f61f805073abe805ac7e3548d6585fb789a08941d716b
-size 12453
+oid sha256:187ab9bb6e4363ec1833547d80ac88c9b3200d2ce9059da7443f06df4d3d6141
+size 12447
