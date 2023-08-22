8464cb80598f35e2ca099f97d9ee36741fa6ab05
Harald Schäfer <harald.the.engineer@gmail.com>
2023-08-21 22:51:02 -0700

    Locationd: switch to laikad (#29278)
    * use_laika
    
    * Update test
    
    * fix spacing
    
    * fix proc replay
    
    * small fix
    
    * Try correct std
    
    * cast correctly
    
    * experimental
    
    * simpler
    
    * update reset thresholds
    
    * improve
    
    * Ready for live running
    
    * outlier reject
    
    * update laika
    
    * add fine speeds
    
    * more experiments
    
    * fixes
    
    * all in laika
    
    * more small tweaks
    
    * outlier rejection not needed
    
    * fix conflict
    
    * no more gpslocation
    
    * update test
    
    * refactor to laika
    
    * runs again
    
    * Update ref
    old-commit-hash: fd3b8a3d7511ad3d7c5aa0b82a1e2c28505e5a7f

--- selfdrive/locationd/laikad.py
+++ selfdrive/locationd/laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f36110abf6e849e414202b6fdba987d2cd439edc5cf582f8c68b29bde3238e7c
-size 20658
+oid sha256:0726d33eb2bad8f760c348bb77dcf3ee93d152564658b6a27200e0f27603aa00
+size 20056
