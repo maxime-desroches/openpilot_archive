50bec453dc8aa355a77c659b10bd1346a5af32a2
Harald Schäfer <harald.the.engineer@gmail.com>
2023-08-22 14:12:51 -0700

    Revert laikad llk (#29535)
    * Revert "bump laika (#29522)"
    
    This reverts commit 19ab859e302d52b4f1667f05543ac622ad259e57.
    
    * Revert "Locationd: switch to laikad (#29278)"
    
    This reverts commit 5dda7be8ac08ef6b51283eda74569517bba905f6.
    old-commit-hash: fda3106c28af236e8b3dc78bfdca7a1365904d1e

--- selfdrive/locationd/test/test_laikad.py
+++ selfdrive/locationd/test/test_laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:886102ba95fc9ba72c2c8be4d8488eb6f5b1c9acdc814f716f6e486ba3cf2c3e
+oid sha256:d6ab76663b46310546b0f9313678caafce85cc18295cb3bc1bf9d7664c537f6b
 size 12536
