049089dd59b9b7b6dd896288639588447c4c0162
Dean Lee <deanlee3@gmail.com>
2023-08-24 05:25:05 +0800

    cpplint: add filter `whitespace/parens` (#29565)
    old-commit-hash: 5480d32bb5d98d57100fd709b9ffe7bfcc6e020a

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd9d77d32ee8980a1286d99f80e743145f1259af384579d1310e5f1c06343d65
-size 19555
+oid sha256:cf8a9ba9391423ac2f33166b180face62ad2d2f9897e0ccae3695910dfa96ae7
+size 19556
