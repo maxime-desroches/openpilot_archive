445cf99b27050dc4f9bc0b53cdf6e80c131dfcaf
Harald Schäfer <harald.the.engineer@gmail.com>
2023-08-23 19:48:55 -0700

    Laikad: ignore more garbage measurements (#29569)
    * Laikad: ignore more garbage measurememnts
    
    * bump laika
    old-commit-hash: f4f8047e979c3366ab4ecd7527cc6daf01f126db

--- selfdrive/locationd/laikad.py
+++ selfdrive/locationd/laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77a36689067c2d6220c2041916df25cb40d84879d672c52c84bab21a02d2d23b
-size 20361
+oid sha256:7919522cc966a65d1257343e6c6ca6cde45a3c3fa49af45057b699b0dca245f3
+size 20671
