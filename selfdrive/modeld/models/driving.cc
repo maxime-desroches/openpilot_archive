a6573089b0bbb669b09f086d58cbe55195e43cef
Mitchell Goff <mitchellgoffpc@gmail.com>
2023-08-25 14:36:26 -0700

    Rewrite modeld in python (#29230)
    * Added modeld.py (WIP)
    
    * No more VisionIpcBufExtra
    
    * Started work on cython bindings for runmodel
    
    * Got ONNXModel cython bindings mostly working, added ModelFrame bindings
    
    * Got modeld main loop running without model eval
    
    * Move everything into ModelState
    
    * Doesn't crash!
    
    * Moved ModelState into modeld.py
    
    * Added driving_pyx
    
    * Added cython bindings for message generation
    
    * Moved CLContext definition to visionipc.pxd
    
    * *facepalm*
    
    * Move cl_pyx into commonmodel_pyx
    
    * Split out ONNXModel into a subclass of RunModel
    
    * Added snpemodel/thneedmodel bindings
    
    * Removed modeld.cc
    
    * Fixed scons for macOS
    
    * Fixed sconscript
    
    * Added flag for thneedmodel
    
    * paths are now relative to openpilot root dir
    
    * Set cl kernel paths in SConscript
    
    * Set LD_PRELOAD=libthneed.so to fix ioctl interception
    
    * Run from root dir
    
    * A few more fixes
    
    * A few more minor fixes
    
    * Use C update_calibration for now to exactly match refs
    
    * Add nav_instructions input
    
    * Link driving_pyx.pyx with transformations
    
    * Checked python FirstOrderFilter against C++ FirstOrderFilter
    
    * Set process name to fix test_onroad
    
    * Revert changes to onnxmodel.cc
    
    * Fixed bad onnx_runner.py path in onnxmodel.cc
    
    * Import all constants from driving.h
    
    * logging -> cloudlog
    
    * pylint import-error suppressions no longer needed?
    
    * Loop in SConscript
    
    * Added parens
    
    * Bump modeld cpu usage in test_onroad
    
    * Get rid of use_nav
    
    * use config_realtime_process
    
    * error message from ioctl sniffer was messing up pyenv
    
    * cast distance_idx to int
    
    * Removed cloudlog.infos in model.run
    
    * Fixed rebase conflicts
    
    * Clean up driving.pxd/pyx
    
    * Fixed linter error
    old-commit-hash: 72a3c987c0d9de6a0cd7a02bba032ccd5e1efe43

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06ea1f70d3880aea41246be4c6289bb34549ca557abaf1987e061ac6ae24572c
-size 19560
+oid sha256:7a582bb4d592a00a402a9cbb1b3fdcff80b8255c3f7a61fb338645f4c5b20fcf
+size 17616
