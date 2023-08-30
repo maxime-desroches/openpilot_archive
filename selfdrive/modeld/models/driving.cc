c6dd5b42defac6d31f3d1811a913dc4a260c536e
Mitchell Goff <mitchellgoffpc@gmail.com>
2023-08-30 14:12:52 -0700

    Move update_calibration to modeld.py (#29688)
    * Move update_calibration to modeld.py
    
    * Updated ref commit
    old-commit-hash: 1eb60516cbcfa014cfd728123b6b3a5b97fce2df

--- selfdrive/modeld/models/driving.cc
+++ selfdrive/modeld/models/driving.cc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a582bb4d592a00a402a9cbb1b3fdcff80b8255c3f7a61fb338645f4c5b20fcf
-size 17616
+oid sha256:ff8397775af4cd1e10c5af3f217a17f66378dd8681e8cc9956e8236694d46074
+size 15579
