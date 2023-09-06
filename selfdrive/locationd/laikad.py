57c59e562f371fbca3e3aed276b391a360f0221c
Dean Lee <deanlee3@gmail.com>
2023-09-06 23:50:28 +0800

    params: safe and efficient async writing parameters (#25912)
    * Safe and efficient asynchronous writing parameters
    
    * call putNonBlocking in locationd
    
    * remove space
    
    * ->AsyncWriter
    
    * remove semicolon
    
    * use member function
    
    * asyc write multiple times
    
    * add test case for AsyncWriter
    
    * merge master
    
    * add missing include
    
    * public
    
    * cleanup
    
    * create once
    
    * cleanup
    
    * update that
    
    * explicit waiting
    
    * improve test case
    
    ---------
    
    Co-authored-by: Adeeb Shihadeh <adeebshihadeh@gmail.com>
    old-commit-hash: 0d797f4e8b2552192669d0329c14849f0a970066

--- selfdrive/locationd/laikad.py
+++ selfdrive/locationd/laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7919522cc966a65d1257343e6c6ca6cde45a3c3fa49af45057b699b0dca245f3
-size 20671
+oid sha256:fd31bcd1d3af98196252239ff5179751c21def19128789f347b5135a61fd4062
+size 20697
