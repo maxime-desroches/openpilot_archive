c60eb61dc924121bc6fa5b1476f068595da151de
Justin Newberry <justin@comma.ai>
2023-09-26 13:44:18 -0700

    CI: use "slow" marker for pytest (#30063)
    * skip to mark
    
    * fix quotes
    old-commit-hash: 0251742079b206ce40a3b4d8ae9cec1381494d4a

--- selfdrive/locationd/test/test_laikad.py
+++ selfdrive/locationd/test/test_laikad.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4a3606603f258bb664ee9caaf3e2fad3f429649ec6ffde7f2d6febb7fb43304
-size 12740
+oid sha256:100127f0e3ac7c7645327c109218abb9551111d3bd87702fac1be858c6958876
+size 12568
